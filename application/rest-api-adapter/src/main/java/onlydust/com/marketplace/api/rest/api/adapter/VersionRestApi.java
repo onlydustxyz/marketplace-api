53877b6fbf7b1aef902925b76db89068e8034ae9
PierreOucif <pierre.oucif@gadz.org>
2023-10-23 10:24:14 +0200

    Adding get version endpoint

--- application/rest-api-adapter/src/main/java/onlydust/com/marketplace/api/rest/api/adapter/VersionRestApi.java
+++ application/rest-api-adapter/src/main/java/onlydust/com/marketplace/api/rest/api/adapter/VersionRestApi.java
@@ -2,10 +2,26 @@ package onlydust.com.marketplace.api.rest.api.adapter;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
+import lombok.AllArgsConstructor;
 import onlydust.com.marketplace.api.contract.VersionApi;
+import onlydust.com.marketplace.api.contract.model.InlineResponse200;
+import onlydust.com.marketplace.api.rest.api.adapter.mapper.DateMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @Tags(@Tag(name = "Version"))
+@AllArgsConstructor
 public class VersionRestApi implements VersionApi {
+
+    private final Date startingDate;
+
+    @Override
+    public ResponseEntity<InlineResponse200> getAPIVersion() {
+        final InlineResponse200 inlineResponse200 = new InlineResponse200();
+        inlineResponse200.setReleaseDate(DateMapper.toZoneDateTime(startingDate));
+        return ResponseEntity.ok(inlineResponse200);
+    }
 }
