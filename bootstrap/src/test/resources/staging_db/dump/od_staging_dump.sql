86789f9401ba023cbfe6749372e9927ab64c4f5d
Anthony Buisset <abuisset@gmail.com>
2023-11-17 10:51:50 +0100

    upload staging dumps to LFS

---
+++ bootstrap/src/test/resources/staging_db/dump/od_staging_dump.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6819eac898c83042126525ce39d6d3ed433106ee17ecfd0423ca7312a720b369
+size 74381537
---
+++ bootstrap/src/test/resources/staging_db/dump/od_staging_indexer_exp_dump.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f93f21d115668a1b385c9a0e913d30929a8c102b843a5436f5598df9ef1ed5f
+size 94010837
