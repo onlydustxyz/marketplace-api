4209ab8878da0d7b535e78e8ed94ec671f21f8f5
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-11-22 10:38:01 +0100

    Migrating registered_users view to new exposition schema

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000005_no_more_old_exp.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000005_no_more_old_exp.sql
@@ -1,6 +1,7 @@
 DROP TABLE projects_contributors;
 DROP TABLE projects_pending_contributors;
 
+
 CREATE VIEW projects_pending_contributors AS
 SELECT pgr.project_id                       AS project_id,
        rc.contributor_id                    AS github_user_id,
@@ -10,7 +11,22 @@ FROM indexer_exp.repos_contributors rc
          JOIN public.project_github_repos pgr on pgr.github_repo_id = rc.repo_id
 GROUP BY pgr.project_id, rc.contributor_id;
 
+
 CREATE VIEW projects_contributors AS
 SELECT *
 FROM projects_pending_contributors
 WHERE completed_contribution_count > 0;
+
+
+DROP VIEW registered_users;
+CREATE VIEW registered_users AS
+SELECT au.id,
+       au.github_user_id,
+       COALESCE(ga.login, au.login_at_signup)                                   AS login,
+       COALESCE(ga.avatar_url, au.avatar_url_at_signup)                         AS avatar_url,
+       COALESCE(ga.html_url, 'https://github.com/'::text || au.login_at_signup) AS html_url,
+       au.email,
+       au.last_seen,
+       au.admin
+FROM auth_users au
+         LEFT JOIN indexer_exp.github_accounts ga ON ga.id = au.github_user_id;
