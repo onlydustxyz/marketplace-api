ebb55f451defc9a26f49a36627937ff9afa9e243
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-11-23 09:42:38 +0100

    Update dump

--- bootstrap/src/test/resources/staging_db/dump/od_staging_indexer_exp_dump.sql
+++ bootstrap/src/test/resources/staging_db/dump/od_staging_indexer_exp_dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:614baea8e32143ce15560f8c650546ed55877bec5c57a3736fe8ed3877daff37
-size 364790524
+oid sha256:49a93ea9c59dc5cedc0d2ac895b74d07985ad4345cd3de1ade9d82ccbf20d962
+size 366489720
