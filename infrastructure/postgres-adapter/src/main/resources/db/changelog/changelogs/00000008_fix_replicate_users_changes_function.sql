4a2b7546922ebf2a76ed0e67c11486a06e6abfe2
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-11-29 16:08:15 +0100

    Fix insert_user_indexing_jobs_from_auth_users_trigger

---
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000008_fix_replicate_users_changes_function.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION public.insert_user_indexing_jobs_from_auth_users()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    -- Insert or update a row into table github_user_indexes when a row is inserted in auth.user_providers
+    INSERT INTO indexer.user_indexing_jobs (user_id)
+    VALUES (NEW.provider_user_id::bigint)
+    ON CONFLICT DO NOTHING;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER insert_user_indexing_jobs_from_auth_users_trigger
+    AFTER INSERT
+    ON auth.user_providers
+    FOR EACH ROW
+EXECUTE FUNCTION public.insert_user_indexing_jobs_from_auth_users();
