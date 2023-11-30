e8df280c73d0fa91b842e622a214773b4cef4d02
Anthony Buisset <abuisset@gmail.com>
2023-11-30 09:08:38 +0100

    new date parser

--- application/rest-api-adapter/src/main/java/onlydust/com/marketplace/api/rest/api/adapter/mapper/DateMapper.java
+++ application/rest-api-adapter/src/main/java/onlydust/com/marketplace/api/rest/api/adapter/mapper/DateMapper.java
@@ -1,7 +1,11 @@
 package onlydust.com.marketplace.api.rest.api.adapter.mapper;
 
+import org.apache.commons.lang3.time.DateParser;
+
+import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import static java.util.Objects.isNull;
@@ -10,5 +14,9 @@ public interface DateMapper {
 
     static ZonedDateTime toZoneDateTime(final Date date) {
         return isNull(date) ? null : ZonedDateTime.ofInstant(date.toInstant(), ZoneId.of("UTC"));
+    }
+
+    static Instant parseInstant(final String date) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd").parse(date, Instant::from);
     }
 }
