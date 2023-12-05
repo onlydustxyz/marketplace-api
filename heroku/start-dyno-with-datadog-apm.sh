9701743476f4fb8650bdc6e647a2ee0fe9598293
PierreOucif <pierre.oucif@gadz.org>
2023-12-05 17:34:43 +0100

    Update procfile to deploy on heroku

--- heroku/start-dyno-with-datadog-apm.sh
+++ heroku/start-dyno-with-datadog-apm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -O dd-java-agent.jar https://dtdg.co/latest-java-tracer
-java -javaagent:dd-java-agent.jar -Dspring.profiles.active=hasura_auth -XX:FlightRecorderOptions=stackdepth=256 -jar bootstrap/target/marketplace-api.jar
+java -javaagent:dd-java-agent.jar -XX:FlightRecorderOptions=stackdepth=256 -jar bootstrap/target/marketplace-api.jar
