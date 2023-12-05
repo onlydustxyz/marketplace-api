52009862f9a5dac80c81d17ec8266eaae5db6e31
PierreOucif <pierre.oucif@gadz.org>
2023-12-05 15:33:33 +0100

    Fix PR comments

--- bootstrap/src/main/java/onlydust/com/marketplace/api/bootstrap/configuration/BackofficeConfiguration.java
+++ bootstrap/src/main/java/onlydust/com/marketplace/api/bootstrap/configuration/BackofficeConfiguration.java
@@ -25,7 +25,6 @@ public class BackofficeConfiguration {
     }
 
     @Bean
-//    @Profile("bo")
     @ConfigurationProperties("application.web.back-office")
     public ApiKeyAuthenticationService.Config apiKeyAuthenticationConfig() {
         return new ApiKeyAuthenticationService.Config();
