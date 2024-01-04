9153738f907a17da0bac61cc1b57db5f591a559b
Anthony Buisset <abuisset@gmail.com>
2024-01-04 14:15:38 +0100

    add equals and hash code

--- domain/src/main/java/onlydust/com/marketplace/api/domain/model/bank/AccountNumber.java
+++ domain/src/main/java/onlydust/com/marketplace/api/domain/model/bank/AccountNumber.java
@@ -1,8 +1,10 @@
 package onlydust.com.marketplace.api.domain.model.bank;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor(staticName = "of")
+@EqualsAndHashCode
 public class AccountNumber {
     private final String value;
 
