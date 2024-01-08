c781e9070adfdde46abac569aa78b5b6a5580751
Anthony Buisset <abuisset@gmail.com>
2024-01-08 10:24:17 +0100

    make currency a record

--- accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/model/Currency.java
+++ accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/model/Currency.java
@@ -1,13 +1,26 @@
 package onlydust.com.marketplace.accounting.domain.model;
 
-import lombok.NonNull;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import onlydust.com.marketplace.kernel.model.UuidWrapper;
 
-public record Currency(@NonNull String code) {
-    public static Currency Usd = new Currency("USD");
-    public static Currency Eth = new Currency("ETH");
+import java.util.UUID;
 
-    @Override
-    public String toString() {
-        return code;
+public record Currency(onlydust.com.marketplace.accounting.domain.model.Currency.Id id, String name, String symbol) {
+    public static Currency of(ERC20 token) {
+        return new Currency(Id.random(), token.name(), token.symbol());
+    }
+
+    @NoArgsConstructor(staticName = "random")
+    @EqualsAndHashCode(callSuper = true)
+    @SuperBuilder
+    public static class Id extends UuidWrapper {
+        public static Id of(@NonNull final UUID uuid) {
+            return Id.builder().uuid(uuid).build();
+        }
+
+        public static Id of(@NonNull final String uuid) {
+            return Id.of(UUID.fromString(uuid));
+        }
     }
 }
