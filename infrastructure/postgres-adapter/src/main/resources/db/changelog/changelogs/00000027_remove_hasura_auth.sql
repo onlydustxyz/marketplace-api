6f4c8fb4dda5d92ac341c4cbd66db7bc42e517a7
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-01-09 17:05:36 +0100

    fix drop trigger

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000027_remove_hasura_auth.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000027_remove_hasura_auth.sql
@@ -7,7 +7,7 @@ drop trigger if exists insert_github_user_indexes_from_auth_users_trigger on aut
 drop trigger if exists insert_user_indexing_jobs_from_auth_users_trigger on auth.user_providers;
 drop trigger if exists replicate_user_providers_changes_trigger on auth.user_providers;
 
-drop trigger if exists replicate_users_changes on auth.users;
+drop trigger if exists replicate_users_changes_trigger on auth.users;
 
 drop function insert_iam_user_from_auth_users();
 drop function update_iam_user_email_from_auth_users();
@@ -17,5 +17,3 @@ drop function insert_github_user_indexes_from_auth_users();
 drop function insert_user_indexing_jobs_from_auth_users();
 drop function replicate_user_providers_changes();
 drop function replicate_users_changes();
-
-
