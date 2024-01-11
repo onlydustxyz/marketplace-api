85c765d1b8dc0615cf5c6b1b533ac916830d21cf
Anthony Buisset <abuisset@gmail.com>
2024-01-11 21:07:25 +0100

    refactor common logic in cmc client

--- infrastructure/coinmarketcap-adapter/src/main/java/onlydust/com/marketplace/api/infrastructure/coinmarketcap/adapters/CmcCurrencyMetadataServiceAdapter.java
+++ infrastructure/coinmarketcap-adapter/src/main/java/onlydust/com/marketplace/api/infrastructure/coinmarketcap/adapters/CmcCurrencyMetadataServiceAdapter.java
@@ -1,14 +1,13 @@
 package onlydust.com.marketplace.api.infrastructure.coinmarketcap.adapters;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.netty.handler.codec.http.HttpMethod;
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.AllArgsConstructor;
 import onlydust.com.marketplace.accounting.domain.model.Currency;
 import onlydust.com.marketplace.accounting.domain.model.ERC20;
 import onlydust.com.marketplace.accounting.domain.port.out.CurrencyMetadataService;
 import onlydust.com.marketplace.api.infrastructure.coinmarketcap.CmcClient;
-import onlydust.com.marketplace.kernel.exception.OnlyDustException;
+import onlydust.com.marketplace.api.infrastructure.coinmarketcap.CmcClient.Response;
 
 import java.net.URI;
 import java.util.Map;
@@ -20,29 +19,17 @@ public class CmcCurrencyMetadataServiceAdapter implements CurrencyMetadataServic
 
     @Override
     public Optional<Currency.Metadata> get(ERC20 token) {
-        return client.send("/v2/cryptocurrency/info?aux=logo,description&address=" + token.address(),
-                        HttpMethod.GET, null, Response.class)
-                .map(this::decode);
+        final var typeRef = new TypeReference<Response<Map<String, Data>>>() {
+        };
+
+        return client.get("/v2/cryptocurrency/info?aux=logo,description&address=" + token.address(), typeRef)
+                .map(d -> d.values().stream().findFirst().orElseThrow().toMetadata());
     }
 
-    private Currency.Metadata decode(Response response) {
-        if (response.status.errorCode > 0)
-            throw OnlyDustException.internalServerError("Unable to fetch token metadata [%d] %s".formatted(
-                    response.status.errorCode, response.status.errorMessage));
-
-        return response.data.values().stream().findFirst().orElseThrow().toMetadata();
-    }
-
-    private record Response(Status status, Map<Integer, Data> data) {
-        @JsonIgnoreProperties(ignoreUnknown = true)
-        private record Status(@JsonProperty("error_code") Integer errorCode, @JsonProperty("error_message") String errorMessage) {
-        }
-
-        @JsonIgnoreProperties(ignoreUnknown = true)
-        private record Data(String description, URI logo) {
-            public Currency.Metadata toMetadata() {
-                return new Currency.Metadata(description, logo);
-            }
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private record Data(String description, URI logo) {
+        public Currency.Metadata toMetadata() {
+            return new Currency.Metadata(description, logo);
         }
     }
 }
--- infrastructure/coinmarketcap-adapter/src/main/java/onlydust/com/marketplace/api/infrastructure/coinmarketcap/adapters/CmcQuoteServiceAdapter.java
+++ infrastructure/coinmarketcap-adapter/src/main/java/onlydust/com/marketplace/api/infrastructure/coinmarketcap/adapters/CmcQuoteServiceAdapter.java
@@ -2,14 +2,13 @@ package onlydust.com.marketplace.api.infrastructure.coinmarketcap.adapters;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.netty.handler.codec.http.HttpMethod;
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.AllArgsConstructor;
 import onlydust.com.marketplace.accounting.domain.model.Currency;
 import onlydust.com.marketplace.accounting.domain.model.ERC20;
 import onlydust.com.marketplace.accounting.domain.model.Quote;
 import onlydust.com.marketplace.accounting.domain.port.out.QuoteService;
 import onlydust.com.marketplace.api.infrastructure.coinmarketcap.CmcClient;
-import onlydust.com.marketplace.kernel.exception.OnlyDustException;
 import onlydust.com.marketplace.kernel.model.blockchain.evm.ContractAddress;
 
 import java.math.BigDecimal;
@@ -29,9 +28,10 @@ public class CmcQuoteServiceAdapter implements QuoteService {
         final var baseId = Optional.ofNullable(properties.currencyIds.get(base))
                 .orElseThrow(() -> badRequest("Currency %s is not supported as base".formatted(base)));
 
-        return client.send("/v2/cryptocurrency/quotes/latest?symbol=%s&convert_id=%d".formatted(token.symbol(), baseId),
-                        HttpMethod.GET, null, Response.class)
-                .map(this::decode)
+        final var typeRef = new TypeReference<CmcClient.Response<Map<String, List<Data>>>>() {
+        };
+
+        return client.get("/v2/cryptocurrency/quotes/latest?symbol=%s&convert_id=%d".formatted(token.symbol(), baseId), typeRef)
                 .flatMap(data -> data.get(token.symbol()).stream()
                         .filter(d -> token.address().equals(d.platform.tokenAddress))
                         .findFirst()
@@ -45,27 +45,14 @@ public class CmcQuoteServiceAdapter implements QuoteService {
         return List.of();
     }
 
-    private Map<String, List<Response.Data>> decode(Response response) {
-        if (response.status.errorCode > 0)
-            throw OnlyDustException.internalServerError("Unable to fetch quotes [%d] %s".formatted(response.status.errorCode, response.status.errorMessage));
-
-        return response.data;
-    }
-
-    private record Response(Status status, Map<String, List<Data>> data) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private record Data(Platform platform, Map<Integer, QuoteResponse> quote) {
         @JsonIgnoreProperties(ignoreUnknown = true)
-        private record Status(@JsonProperty("error_code") Integer errorCode, @JsonProperty("error_message") String errorMessage) {
+        private record QuoteResponse(BigDecimal price) {
         }
 
         @JsonIgnoreProperties(ignoreUnknown = true)
-        private record Data(Platform platform, Map<Integer, QuoteResponse> quote) {
-            @JsonIgnoreProperties(ignoreUnknown = true)
-            private record QuoteResponse(BigDecimal price) {
-            }
-
-            @JsonIgnoreProperties(ignoreUnknown = true)
-            private record Platform(@JsonProperty("token_address") ContractAddress tokenAddress) {
-            }
+        private record Platform(@JsonProperty("token_address") ContractAddress tokenAddress) {
         }
     }
 
