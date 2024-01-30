585af619dce89aca67a2595762f7e8c1f8eb0a2a
Anthony Buisset <abuisset@gmail.com>
2024-01-30 14:48:42 +0100

    change contributor id to store a long

--- accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/model/ContributorId.java
+++ accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/model/ContributorId.java
@@ -1,22 +1,20 @@
 package onlydust.com.marketplace.accounting.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.experimental.SuperBuilder;
-import onlydust.com.marketplace.kernel.model.UuidWrapper;
 
-import java.util.UUID;
-
-@NoArgsConstructor(staticName = "random")
-@EqualsAndHashCode(callSuper = true)
-@SuperBuilder
-public class ContributorId extends UuidWrapper {
-    public static ContributorId of(@NonNull final UUID uuid) {
-        return ContributorId.builder().uuid(uuid).build();
-    }
+@EqualsAndHashCode
+@AllArgsConstructor(staticName = "of")
+public class ContributorId {
+    @NonNull
+    private final Long value;
 
     public static ContributorId of(@NonNull final String uuid) {
-        return ContributorId.of(UUID.fromString(uuid));
+        return ContributorId.of(Long.parseLong(uuid));
+    }
+
+    public Long value() {
+        return value;
     }
 }
