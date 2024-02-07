7e645a07a68780d6c1d03d1532128d9e1a7ac543
PierreOucif <pierre.oucif@gadz.org>
2024-02-07 17:55:36 +0100

    Update DB scripts for ITs

--- bootstrap/src/test/resources/database/scripts/restore_db.sh
+++ bootstrap/src/test/resources/database/scripts/restore_db.sh
@@ -8,7 +8,7 @@ fi
 
 # Restore the data
 # To create the dump, run the following command in docker:
-#   > pg_dump -d marketplace_db -U test --schema='public' --schema='iam' --schema='rfd' --data-only -Fc -f /tmp/IT_data.dump
+#   > pg_dump -d marketplace_db -U test --schema='public' --schema='iam' --schema='rfd' --schema='accounting' --data-only -Fc -f /tmp/IT_data.dump
 if ! pg_restore -d marketplace_db -U test -j 6 --disable-triggers /tmp/IT_data.dump; then
   echo "Failed to restore data"
   exit 1
