92d0792ce39406720cd31f7449c29f8191957c15
PierreOucif <pierre.oucif@gadz.org>
2024-02-07 15:40:32 +0100

    Migrating payout info to billing profile

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000040_create_billing_profile_tables.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000040_create_billing_profile_tables.sql
@@ -44,5 +44,58 @@ CREATE TABLE user_billing_profile_types
     billing_profile_type billing_profile_type NOT NULL,
     created_at           TIMESTAMP            NOT NULL DEFAULT NOW(),
     updated_at           TIMESTAMP            NOT NULL DEFAULT NOW()
-)
+);
 
+insert into user_billing_profile_types (user_id, billing_profile_type)
+select upi.user_id, 'INDIVIDUAL'
+from user_payout_info upi
+where upi.identity -> 'Person' is not null;
+
+insert into user_billing_profile_types (user_id, billing_profile_type)
+select upi.user_id, 'COMPANY'
+from user_payout_info upi
+where upi.identity -> 'Company' is not null;
+
+with individual_profile
+         as (select upi.identity -> 'Person' ->> 'firstname' first_name,
+                    upi.identity -> 'Person' ->> 'lastname'  last_name,
+                    upi.location ->> 'city'                  city,
+                    upi.location ->> 'address'               address,
+                    upi.location ->> 'country'               country,
+                    upi.location ->> 'post_code'             post_code,
+                    upi.user_id
+             from user_payout_info upi
+             where upi.identity -> 'Person' is not null)
+insert
+into individual_billing_profiles (id, user_id, verification_status, first_name, last_name, country, address)
+select gen_random_uuid(),
+       ip.user_id,
+       'NOT_STARTED',
+       ip.first_name,
+       ip.last_name,
+       ip.country,
+       ip.address || ', ' || ip.post_code || ', ' || ip.city || ', ' || ip.country
+from individual_profile ip;
+
+
+
+with company_profile
+         as (select upi.identity -> 'Company' ->> 'name'                  name,
+                    upi.identity -> 'Company' ->> 'identification_number' id_number,
+                    upi.location ->> 'city'                               city,
+                    upi.location ->> 'address'                            address,
+                    upi.location ->> 'country'                            country,
+                    upi.location ->> 'post_code'                          post_code,
+                    upi.user_id
+             from user_payout_info upi
+             where upi.identity -> 'Company' is not null)
+insert
+into company_billing_profiles (id, user_id, verification_status, name, registration_number, country, address)
+select gen_random_uuid(),
+       cp.user_id,
+       'NOT_STARTED',
+       cp.name,
+       cp.id_number,
+       cp.country,
+       cp.address || ', ' || cp.post_code || ', ' || cp.city || ', ' || cp.country
+from company_profile cp;
