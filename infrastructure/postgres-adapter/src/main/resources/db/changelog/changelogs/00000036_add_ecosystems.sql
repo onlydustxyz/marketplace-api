64c6ded8cda7a220d11e8eddc6ef7f48c349d128
Ilysse <pierre.oucif@gadz.org>
2024-02-08 17:26:07 +0100

    Fix ecosystems migration (#337)
    * Warning : migration checksum to update manualy
    
    * Updating dump

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000036_add_ecosystems.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000036_add_ecosystems.sql
@@ -31,46 +31,5 @@ VALUES ('Optimism', 'https://onlydust-app-images.s3.eu-west-1.amazonaws.com/1205
 INSERT INTO ecosystems (name, logo_url, url)
 VALUES ('Avail', 'https://onlydust-app-images.s3.eu-west-1.amazonaws.com/12011103528231014365.png', 'https://www.availproject.org/');
 
-INSERT INTO projects_ecosystems as pe (project_id, ecosystem_id)
-select ps.project_id, (select e.id from ecosystems e where e.name = 'Zama')
-from projects_sponsors ps
-         join sponsors s on s.id = ps.sponsor_id
-where s.name = 'Zama';
-
-INSERT INTO projects_ecosystems as pe (project_id, ecosystem_id)
-select ps.project_id, (select e.id from ecosystems e where e.name = 'Starknet')
-from projects_sponsors ps
-         join sponsors s on s.id = ps.sponsor_id
-where s.name in ('Starkware Exploration Team', 'Voyager', 'Starkware', 'Starknet Foundation', 'Kakarot', 'Kasar Labs');
-
-INSERT INTO projects_ecosystems as pe (project_id, ecosystem_id)
-select ps.project_id, (select e.id from ecosystems e where e.name = 'Ethereum')
-from projects_sponsors ps
-         join sponsors s on s.id = ps.sponsor_id
-where s.name in ('Paradigm', 'Ethereum Foundation');
-
-INSERT INTO projects_ecosystems as pe (project_id, ecosystem_id)
-select ps.project_id, (select e.id from ecosystems e where e.name = 'Aptos')
-from projects_sponsors ps
-         join sponsors s on s.id = ps.sponsor_id
-where s.name = 'Aptos Foundation';
-
-INSERT INTO projects_ecosystems as pe (project_id, ecosystem_id)
-select ps.project_id, (select e.id from ecosystems e where e.name = 'Aztec')
-from projects_sponsors ps
-         join sponsors s on s.id = ps.sponsor_id
-where s.name = 'Aztec Protocol';
-
-INSERT INTO projects_ecosystems as pe (project_id, ecosystem_id)
-select ps.project_id, (select e.id from ecosystems e where e.name = 'Optimism')
-from projects_sponsors ps
-         join sponsors s on s.id = ps.sponsor_id
-where s.name = 'Optimism';
-
-INSERT INTO projects_ecosystems as pe (project_id, ecosystem_id)
-select ps.project_id, (select e.id from ecosystems e where e.name = 'Avail')
-from projects_sponsors ps
-         join sponsors s on s.id = ps.sponsor_id
-where s.name = 'Avail';
 
 
