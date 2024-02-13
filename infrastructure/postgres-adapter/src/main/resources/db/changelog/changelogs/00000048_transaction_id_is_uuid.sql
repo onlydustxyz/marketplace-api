b6e4e5c1914888673483a2be1125edd84d8834ae
Anthony Buisset <abuisset@gmail.com>
2024-02-13 09:57:49 +0100

    fix migration

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000048_transaction_id_is_uuid.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000048_transaction_id_is_uuid.sql
@@ -2,7 +2,10 @@ ALTER TABLE accounting.sponsor_account_transactions
     DROP COLUMN id;
 
 ALTER TABLE accounting.sponsor_account_transactions
-    ADD COLUMN id UUID;
+    ADD COLUMN id UUID DEFAULT gen_random_uuid();
 
 ALTER TABLE accounting.sponsor_account_transactions
     ADD PRIMARY KEY (id);
+
+ALTER TABLE accounting.sponsor_account_transactions
+    ALTER COLUMN id DROP DEFAULT;
