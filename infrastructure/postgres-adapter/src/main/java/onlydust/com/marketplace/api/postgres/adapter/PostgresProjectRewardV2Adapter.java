5a5e5cad513e5ff2968b6ed7d52d44a436ec72e2
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-02-13 09:54:28 +0100

    Implement PostgresProjectRewardV2Adapter.findBudgets

---
+++ infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/PostgresProjectRewardV2Adapter.java
@@ -0,0 +1,71 @@
+package onlydust.com.marketplace.api.postgres.adapter;
+
+import lombok.AllArgsConstructor;
+import onlydust.com.marketplace.api.domain.port.output.ProjectRewardStoragePort;
+import onlydust.com.marketplace.api.domain.view.*;
+import onlydust.com.marketplace.api.domain.view.pagination.Page;
+import onlydust.com.marketplace.api.domain.view.pagination.SortDirection;
+import onlydust.com.marketplace.api.postgres.adapter.entity.write.HistoricalQuoteEntity;
+import onlydust.com.marketplace.api.postgres.adapter.repository.CurrencyRepository;
+import onlydust.com.marketplace.api.postgres.adapter.repository.HistoricalQuoteRepository;
+import onlydust.com.marketplace.api.postgres.adapter.repository.ProjectAllowanceRepository;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.UUID;
+
+import static onlydust.com.marketplace.kernel.exception.OnlyDustException.internalServerError;
+
+@AllArgsConstructor
+public class PostgresProjectRewardV2Adapter implements ProjectRewardStoragePort {
+
+    private final ProjectAllowanceRepository projectAllowanceRepository;
+    private final HistoricalQuoteRepository historicalQuoteRepository;
+    private final CurrencyRepository currencyRepository;
+
+    @Override
+    @Transactional
+    public ProjectRewardsPageView findRewards(UUID projectId, ProjectRewardView.Filters filters, ProjectRewardView.SortBy sortBy, SortDirection sortDirection
+            , int pageIndex, int pageSize) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public ProjectBudgetsView findBudgets(UUID projectId) {
+        final var usd = currencyRepository.findByCode("USD").orElseThrow(() -> internalServerError("USD currency not found"));
+
+        final var budgets = projectAllowanceRepository.findAllByProjectId(projectId).stream().map(projectAllowanceEntity -> {
+            final var quote = historicalQuoteRepository.findFirstByCurrencyIdAndBaseIdAndTimestampLessThanEqualOrderByTimestampDesc(
+                            projectAllowanceEntity.getCurrencyId(),
+                            usd.id(),
+                            Instant.now()
+                    ).map(HistoricalQuoteEntity::toDomain)
+                    .orElseThrow(() -> internalServerError("No quote found for currency %s and base %s"
+                            .formatted(projectAllowanceEntity.getCurrencyId(), usd.id())));
+            return BudgetView.builder()
+                    .remaining(projectAllowanceEntity.getCurrentAllowance())
+                    .initialAmount(projectAllowanceEntity.getInitialAllowance())
+                    .remainingDollarsEquivalent(quote.convertToBaseCurrency(projectAllowanceEntity.getCurrentAllowance()))
+                    .initialDollarsEquivalent(quote.convertToBaseCurrency(projectAllowanceEntity.getInitialAllowance()))
+                    .build();
+        }).toList();
+
+        return ProjectBudgetsView.builder().budgets(budgets).build();
+    }
+
+    @Override
+    @Transactional
+    public RewardView getProjectReward(UUID rewardId) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Page<RewardItemView> getProjectRewardItems(UUID rewardId, int pageIndex, int pageSize) {
+        // TODO
+        return null;
+    }
+}
