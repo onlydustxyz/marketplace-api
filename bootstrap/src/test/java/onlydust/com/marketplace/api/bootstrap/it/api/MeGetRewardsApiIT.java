c61642797c226f567384f6c088e0538b4ead693a
PierreOucif <pierre.oucif@gadz.org>
2024-02-14 17:40:02 +0100

    Fixing reward status

--- bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/MeGetRewardsApiIT.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/MeGetRewardsApiIT.java
@@ -73,122 +73,125 @@ public class MeGetRewardsApiIT extends AbstractMarketplaceApiIT {
             }
             """;
     private static final String GET_USER_REWARDS_WITH_MULTI_CURRENCIES_RESPONSE_JSON = """
+           
             {
-              "rewards": [
-                {
-                  "requestedAt": "2023-09-19T07:38:22.018458Z",
-                  "processedAt": null,
-                  "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
-                  "status": "MISSING_PAYOUT_INFO",
-                  "unlockDate": null,
-                  "amount": {
-                    "total": 500,
-                    "currency": "APT",
-                    "dollarsEquivalent": 100000
-                  },
-                  "numberOfRewardedContributions": 25,
-                  "rewardedOnProjectName": "QA new contributions",
-                  "rewardedOnProjectLogoUrl": null,
-                  "id": "2ac80cc6-7e83-4eef-bc0c-932b58f683c0"
-                },
-                {
-                  "requestedAt": "2023-09-20T08:46:52.77875Z",
-                  "processedAt": null,
-                  "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
-                  "status": "PENDING_INVOICE",
-                  "unlockDate": null,
-                  "amount": {
-                    "total": 50,
-                    "currency": "ETH",
-                    "dollarsEquivalent": 75000
-                  },
-                  "numberOfRewardedContributions": 1,
-                  "rewardedOnProjectName": "QA new contributions",
-                  "rewardedOnProjectLogoUrl": null,
-                  "id": "e1498a17-5090-4071-a88a-6f0b0c337c3a"
-                },
-                {
-                  "requestedAt": "2023-09-19T07:40:26.971981Z",
-                  "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
-                  "status": "COMPLETE",
-                  "unlockDate": null,
-                  "amount": {
-                    "total": 10,
-                    "currency": "ETH",
-                    "dollarsEquivalent": 15000
-                  },
-                  "numberOfRewardedContributions": 25,
-                  "rewardedOnProjectName": "QA new contributions",
-                  "rewardedOnProjectLogoUrl": null,
-                  "id": "40fda3c6-2a3f-4cdd-ba12-0499dd232d53"
-                },
-                {
-                  "requestedAt": "2023-09-19T07:38:52.590518Z",
-                  "processedAt": null,
-                  "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
-                  "status": "PENDING_INVOICE",
-                  "unlockDate": null,
-                  "amount": {
-                    "total": 1000,
-                    "currency": "USDC",
-                    "dollarsEquivalent": 1010.00
-                  },
-                  "numberOfRewardedContributions": 25,
-                  "rewardedOnProjectName": "QA new contributions",
-                  "rewardedOnProjectLogoUrl": null,
-                  "id": "85f8358c-5339-42ac-a577-16d7760d1e28"
-                },
-                {
-                  "requestedAt": "2023-09-19T07:39:54.45638Z",
-                  "processedAt": null,
-                  "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
-                  "status": "LOCKED",
-                  "unlockDate": "2024-08-23T00:00:00Z",
-                  "amount": {
-                    "total": 30,
-                    "currency": "OP",
-                    "dollarsEquivalent": null
-                  },
-                  "numberOfRewardedContributions": 25,
-                  "rewardedOnProjectName": "QA new contributions",
-                  "rewardedOnProjectLogoUrl": null,
-                  "id": "8fe07ae1-cf3b-4401-8958-a9e0b0aec7b0"
-                },
-                {
-                  "requestedAt": "2023-09-19T07:39:23.730967Z",
-                  "processedAt": null,
-                  "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
-                  "status": "LOCKED",
-                  "unlockDate": null,
-                  "amount": {
-                    "total": 9511147,
-                    "currency": "STRK",
-                    "dollarsEquivalent": null
-                  },
-                  "numberOfRewardedContributions": 25,
-                  "rewardedOnProjectName": "QA new contributions",
-                  "rewardedOnProjectLogoUrl": null,
-                  "id": "5b96ca1e-4ad2-41c1-8819-520b885d9223"
-                }
-              ],
-              "hasMore": false,
-              "totalPageNumber": 1,
-              "totalItemNumber": 6,
-              "nextPageIndex": 0,
-              "rewardedAmount": {
-                "amount": null,
-                "currency": null,
-                "usdEquivalent": 191010.00
-              },
-              "pendingAmount": {
-                "amount": null,
-                "currency": null,
-                "usdEquivalent": 176010.00
-              },
-              "receivedRewardsCount": 6,
-              "rewardedContributionsCount": 26,
-              "rewardingProjectsCount": 1
-            }            
+             "rewards": [
+               {
+                 "requestedAt": "2023-09-19T07:38:22.018458Z",
+                 "processedAt": null,
+                 "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
+                 "status": "MISSING_PAYOUT_INFO",
+                 "unlockDate": null,
+                 "amount": {
+                   "total": 500,
+                   "currency": "APT",
+                   "dollarsEquivalent": 100000
+                 },
+                 "numberOfRewardedContributions": 25,
+                 "rewardedOnProjectName": "QA new contributions",
+                 "rewardedOnProjectLogoUrl": null,
+                 "id": "2ac80cc6-7e83-4eef-bc0c-932b58f683c0"
+               },
+               {
+                 "requestedAt": "2023-09-20T08:46:52.77875Z",
+                 "processedAt": null,
+                 "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
+                 "status": "PENDING_INVOICE",
+                 "unlockDate": null,
+                 "amount": {
+                   "total": 50,
+                   "currency": "ETH",
+                   "dollarsEquivalent": 75000
+                 },
+                 "numberOfRewardedContributions": 1,
+                 "rewardedOnProjectName": "QA new contributions",
+                 "rewardedOnProjectLogoUrl": null,
+                 "id": "e1498a17-5090-4071-a88a-6f0b0c337c3a"
+               },
+               {
+                 "requestedAt": "2023-09-19T07:40:26.971981Z",
+                 "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
+                 "status": "COMPLETE",
+                 "unlockDate": null,
+                 "amount": {
+                   "total": 10,
+                   "currency": "ETH",
+                   "dollarsEquivalent": 15000
+                 },
+                 "numberOfRewardedContributions": 25,
+                 "rewardedOnProjectName": "QA new contributions",
+                 "rewardedOnProjectLogoUrl": null,
+                 "id": "40fda3c6-2a3f-4cdd-ba12-0499dd232d53"
+               },
+               {
+                 "requestedAt": "2023-09-19T07:38:52.590518Z",
+                 "processedAt": null,
+                 "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
+                 "status": "PENDING_INVOICE",
+                 "unlockDate": null,
+                 "amount": {
+                   "total": 1000,
+                   "currency": "USDC",
+                   "dollarsEquivalent": 1010.00
+                 },
+                 "numberOfRewardedContributions": 25,
+                 "rewardedOnProjectName": "QA new contributions",
+                 "rewardedOnProjectLogoUrl": null,
+                 "id": "85f8358c-5339-42ac-a577-16d7760d1e28"
+               },
+               {
+                 "requestedAt": "2023-09-19T07:39:54.45638Z",
+                 "processedAt": null,
+                 "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
+                 "status": "MISSING_PAYOUT_INFO",
+                 "unlockDate": "2024-08-23T00:00:00Z",
+                 "amount": {
+                   "total": 30,
+                   "currency": "OP",
+                   "dollarsEquivalent": null
+                 },
+                 "numberOfRewardedContributions": 25,
+                 "rewardedOnProjectName": "QA new contributions",
+                 "rewardedOnProjectLogoUrl": null,
+                 "id": "8fe07ae1-cf3b-4401-8958-a9e0b0aec7b0"
+               },
+               {
+                 "requestedAt": "2023-09-19T07:39:23.730967Z",
+                 "processedAt": null,
+                 "projectId": "f39b827f-df73-498c-8853-99bc3f562723",
+                 "status": "MISSING_PAYOUT_INFO",
+                 "unlockDate": null,
+                 "amount": {
+                   "total": 9511147,
+                   "currency": "STRK",
+                   "dollarsEquivalent": null
+                 },
+                 "numberOfRewardedContributions": 25,
+                 "rewardedOnProjectName": "QA new contributions",
+                 "rewardedOnProjectLogoUrl": null,
+                 "id": "5b96ca1e-4ad2-41c1-8819-520b885d9223"
+               }
+             ],
+             "hasMore": false,
+             "totalPageNumber": 1,
+             "totalItemNumber": 6,
+             "nextPageIndex": 0,
+             "rewardedAmount": {
+               "amount": null,
+               "currency": null,
+               "usdEquivalent": 191010.00
+             },
+             "pendingAmount": {
+               "amount": null,
+               "currency": null,
+               "usdEquivalent": 176010.00
+             },
+             "receivedRewardsCount": 6,
+             "rewardedContributionsCount": 26,
+             "rewardingProjectsCount": 1
+
+                      }
+           
             """;
     private static final String ME_GET_REWARDS_RESPONSE_JSON = """
             {
