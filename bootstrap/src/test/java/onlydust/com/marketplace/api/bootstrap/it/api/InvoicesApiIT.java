5a8fa88b92907dd98bbeaa365e936c722089af30
Anthony Buisset <abuisset@gmail.com>
2024-02-15 15:53:48 +0100

    better checks

--- bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/InvoicesApiIT.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/InvoicesApiIT.java
@@ -1,17 +1,11 @@
 package onlydust.com.marketplace.api.bootstrap.it.api;
 
-import onlydust.com.marketplace.api.contract.model.BillingProfileType;
-import onlydust.com.marketplace.api.contract.model.CurrencyContract;
 import onlydust.com.marketplace.api.contract.model.MyBillingProfilesResponse;
-import onlydust.com.marketplace.api.contract.model.NewInvoiceResponse;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
 import java.util.Map;
 
 import static onlydust.com.marketplace.api.rest.api.adapter.authentication.AuthenticationFilter.BEARER_PREFIX;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class InvoicesApiIT extends AbstractMarketplaceApiIT {
 
@@ -34,7 +28,7 @@ public class InvoicesApiIT extends AbstractMarketplaceApiIT {
         final var billingProfileId = myBillingProfiles.getBillingProfiles().get(0).getId();
 
         // When
-        final var response = client.get()
+        client.get()
                 .uri(getApiURI(BILLING_PROFILE_INVOICE_PREVIEW.formatted(billingProfileId), Map.of(
                         "pageIndex", "0",
                         "pageSize", "10",
@@ -45,40 +39,80 @@ public class InvoicesApiIT extends AbstractMarketplaceApiIT {
                 // Then
                 .expectStatus()
                 .is2xxSuccessful()
-                .expectBody(NewInvoiceResponse.class)
-                .returnResult().getResponseBody();
-
-        assertThat(response).isNotNull();
-        assertThat(response.getId()).isEqualTo("OD-BUISSET-ANTHONY-001");
-        assertThat(response.getCreatedAt()).isBefore(ZonedDateTime.now());
-        assertThat(response.getDueAt()).isAfter(ZonedDateTime.now());
-        assertThat(response.getBillingProfileType()).isEqualTo(BillingProfileType.INDIVIDUAL);
-        assertThat(response.getIndividualBillingProfile().getFirstName()).isEqualTo("Anthony");
-        assertThat(response.getIndividualBillingProfile().getLastName()).isEqualTo("BUISSET");
-        assertThat(response.getIndividualBillingProfile().getAddress()).isEqualTo("771 chemin de la sine, 06140, Vence, France");
-        assertThat(response.getCompanyBillingProfile()).isNull();
-        assertThat(response.getDestinationAccounts()).isNull();
-        assertThat(response.getRewards()).hasSize(3);
-        assertThat(response.getRewards().stream().filter(r -> r.getAmount().getCurrency() == CurrencyContract.ETH)).hasSize(1);
-        assertThat(response.getRewards().stream().filter(r -> r.getAmount().getCurrency() == CurrencyContract.USDC)).hasSize(2);
-        assertThat(response.getRewards()).allMatch(r -> r.getId() != null);
-        assertThat(response.getRewards()).allMatch(r -> r.getDate() != null);
-        assertThat(response.getRewards()).allMatch(r -> r.getProjectName() != null);
-        assertThat(response.getRewards()).allMatch(r -> r.getAmount().getAmount().compareTo(BigDecimal.ZERO) > 0);
-        assertThat(response.getRewards()).allMatch(r -> r.getAmount().getCurrency() != null);
-        assertThat(response.getRewards()).allMatch(r -> r.getAmount().getBase().getAmount().compareTo(BigDecimal.ZERO) > 0);
-        assertThat(response.getRewards()).allMatch(r -> r.getAmount().getBase().getCurrency() == CurrencyContract.USD);
-        assertThat(response.getRewards().stream().filter(r -> r.getAmount().getCurrency() == CurrencyContract.ETH))
-                .allMatch(r -> r.getAmount().getBase().getConversionRate().equals(BigDecimal.valueOf(1781.98)));
-        assertThat(response.getRewards().stream().filter(r -> r.getAmount().getCurrency() == CurrencyContract.USDC))
-                .allMatch(r -> r.getAmount().getBase().getConversionRate().equals(BigDecimal.valueOf(1.01)));
-        assertThat(response.getTotalBeforeTax().getAmount().equals(BigDecimal.valueOf(1784757.5)));
-        assertThat(response.getTotalBeforeTax().getCurrency()).isEqualTo(CurrencyContract.USD);
-        assertThat(response.getTaxRate()).isEqualTo(BigDecimal.ZERO);
-        assertThat(response.getTotalTax().getAmount()).isEqualByComparingTo(BigDecimal.ZERO);
-        assertThat(response.getTotalTax().getCurrency()).isEqualTo(CurrencyContract.USD);
-        assertThat(response.getTotalAfterTax().getAmount()).isEqualTo(response.getTotalBeforeTax().getAmount());
-        assertThat(response.getTotalAfterTax().getCurrency()).isEqualTo(CurrencyContract.USD);
+                .expectBody()
+                .jsonPath("$.createdAt").isNotEmpty()
+                .jsonPath("$.dueAt").isNotEmpty()
+                .json("""
+                        {
+                          "id": "OD-BUISSET-ANTHONY-001",
+                          "billingProfileType": "INDIVIDUAL",
+                          "individualBillingProfile": {
+                            "firstName": "Anthony",
+                            "lastName": "BUISSET",
+                            "address": "771 chemin de la sine, 06140, Vence, France"
+                          },
+                          "companyBillingProfile": null,
+                          "destinationAccounts": null,
+                          "rewards": [
+                            {
+                              "id": "79209029-c488-4284-aa3f-bce8870d3a66",
+                              "date": "2023-06-02T08:49:08.444047Z",
+                              "projectName": "kaaper",
+                              "amount": {
+                                "amount": 1000,
+                                "currency": "USDC",
+                                "base": {
+                                  "amount": 1010.00,
+                                  "currency": "USD",
+                                  "conversionRate": 1.01
+                                }
+                              }
+                            },
+                            {
+                              "id": "ee28315c-7a84-4052-9308-c2236eeafda1",
+                              "date": "2023-06-22T08:47:12.915468Z",
+                              "projectName": "Aldébaran du Taureau",
+                              "amount": {
+                                "amount": 1750,
+                                "currency": "USDC",
+                                "base": {
+                                  "amount": 1767.50,
+                                  "currency": "USD",
+                                  "conversionRate": 1.01
+                                }
+                              }
+                            },
+                            {
+                              "id": "966cd55c-7de8-45c4-8bba-b388c38ca15d",
+                              "date": "2023-06-02T08:48:04.697886Z",
+                              "projectName": "kaaper",
+                              "amount": {
+                                "amount": 1000,
+                                "currency": "ETH",
+                                "base": {
+                                  "amount": 1781980.00,
+                                  "currency": "USD",
+                                  "conversionRate": 1781.98
+                                }
+                              }
+                            }
+                          ],
+                          "totalBeforeTax": {
+                            "amount": 1784757.50,
+                            "currency": "USD"
+                          },
+                          "taxRate": 0,
+                          "totalTax": {
+                            "amount": 0.00,
+                            "currency": "USD"
+                          },
+                          "totalAfterTax": {
+                            "amount": 1784757.50,
+                            "currency": "USD"
+                          }
+                        }
+                        """
+                );
 
         // When
         client.get()
