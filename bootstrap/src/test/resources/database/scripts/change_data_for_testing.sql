e187d5deeb490ece9f1d3ea2e23269a2d4f55731
Anthony Buisset <abuisset@gmail.com>
2024-02-28 18:01:02 +0100

    update testing dataset

--- bootstrap/src/test/resources/database/scripts/change_data_for_testing.sql
+++ bootstrap/src/test/resources/database/scripts/change_data_for_testing.sql
@@ -25,3 +25,7 @@ SELECT now(), usd.id, eur.id, 0.92
 FROM currencies usd
          JOIN currencies eur ON eur.code = 'EUR'
 WHERE usd.code = 'USD';
+
+insert into currencies (id, type, name, code, logo_url, decimals, description)
+values ('48388edb-fda2-4a32-b228-28152a147500', 'CRYPTO', 'Aptos Coin', 'APT', null, 8, null),
+       ('00ca98a5-0197-4b76-a208-4bfc55ea8256', 'CRYPTO', 'Optimism', 'OP', null, 18, null);
