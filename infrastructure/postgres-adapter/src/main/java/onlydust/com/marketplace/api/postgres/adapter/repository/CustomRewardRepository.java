13510c68c9abaf90b3290b23bcca854a7e899c15
Anthony Buisset <abuisset@gmail.com>
2024-03-01 18:49:37 +0100

    migrate reward items

--- infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/CustomRewardRepository.java
+++ infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/CustomRewardRepository.java
@@ -11,10 +11,10 @@ import java.util.UUID;
 @AllArgsConstructor
 public class CustomRewardRepository {
     private static final String COUNT_REWARD_ITEMS = """
-            select count(distinct wi.id)
-            from payment_requests pr
-                     join public.work_items wi on wi.payment_id = pr.id
-            where pr.id = :rewardId""";
+            select count(distinct ri.id)
+            from rewards r
+                     join reward_items ri on ri.reward_id = r.id
+            where r.id = :rewardId""";
     private static final String FIND_REWARD_ITEMS = """
             with get_pr as              (select gpr.number,
                                                gpr.id,
@@ -62,7 +62,7 @@ public class CustomRewardRepository {
                                               gprr.author_html_url       author_github_url,
                                               gprr.body                  github_body
                                        from indexer_exp.github_code_reviews gprr)
-            select distinct wi.type,
+            select distinct ri.type,
                             coalesce(cast(pull_request.id as text), cast(issue.id as text), cast(code_review.id as text))             reward_id,
                             c.id                                                                                                      contribution_id,
                             coalesce(cast(pull_request.status as text), cast(issue.status as text), cast(code_review.status as text)) status,
@@ -82,21 +82,21 @@ public class CustomRewardRepository {
                             pull_request.commits_count,
                             (select gprcc.commit_count
                              from indexer_exp.github_pull_request_commit_counts gprcc
-                             where gprcc.pull_request_id = pull_request.id and gprcc.author_id = pr.recipient_id) user_commits_count,
+                             where gprcc.pull_request_id = pull_request.id and gprcc.author_id = r.recipient_id) user_commits_count,
                             issue.comments_count,
-                            pr.recipient_id
-            from payment_requests pr
-                     join public.work_items wi on wi.payment_id = pr.id
-                     left join get_issue issue on issue.id = (case when wi.id ~ '^[0-9]+$' then cast(wi.id as bigint) else -1 end)
-                     left join get_pr pull_request on pull_request.id = (case when wi.id ~ '^[0-9]+$' then cast(wi.id as bigint) else -1 end)
-                     left join get_code_review code_review on code_review.id = wi.id
-                     left join indexer_exp.contributions c on c.contributor_id = pr.recipient_id and c.repo_id = wi.repo_id and
+                            r.recipient_id
+            from rewards r
+                     join reward_items ri on ri.reward_id = r.id
+                     left join get_issue issue on issue.id = (case when ri.id ~ '^[0-9]+$' then cast(ri.id as bigint) else -1 end)
+                     left join get_pr pull_request on pull_request.id = (case when ri.id ~ '^[0-9]+$' then cast(ri.id as bigint) else -1 end)
+                     left join get_code_review code_review on code_review.id = ri.id
+                     left join indexer_exp.contributions c on c.contributor_id = r.recipient_id and c.repo_id = ri.repo_id and
                                                               -- check that the repo is still part of the project (if it's not, then the contribution doesn't belong to the project)
-                                                              exists (select 1 from project_github_repos pgr where pgr.github_repo_id = c.repo_id and pgr.project_id = pr.project_id) and
+                                                              exists (select 1 from project_github_repos pgr where pgr.github_repo_id = c.repo_id and pgr.project_id = r.project_id) and
                                                               ((issue.id IS NOT NULL and c.issue_id = issue.id) or
                                                               (pull_request.id IS NOT NULL and c.pull_request_id = pull_request.id) or
                                                               (code_review.id IS NOT NULL and c.code_review_id = code_review.id))
-            where pr.id = :rewardId
+            where r.id = :rewardId
             order by start_date desc, end_date desc offset :offset limit :limit""";
 
     private final EntityManager entityManager;
