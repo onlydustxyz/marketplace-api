47b9246db97ae9bad4b57cfaafb62a515febe5ca
Anthony Buisset <abuisset@gmail.com>
2024-03-11 11:09:38 +0100

    fix migration

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000100_invoice_data_v2.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000100_invoice_data_v2.sql
@@ -2,7 +2,7 @@ ALTER TABLE accounting.invoices
     ADD COLUMN created_by UUID;
 
 UPDATE accounting.invoices
-SET created_by = (select bpu.user_id from accounting.billing_profiles_users bpu where bpu.billing_profile_id = billing_profile_id),
+SET created_by = (select bpu.user_id from accounting.billing_profiles_users bpu where bpu.billing_profile_id = billing_profile_id limit 1),
     data       =
         jsonb_build_object
         (
