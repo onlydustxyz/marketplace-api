81e2b86fbbdcf217434101e70f081d8ec88c3728
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-03-19 12:06:00 +0100

    Remake a test

--- bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/ProjectDeleteRewardsApiIT.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/ProjectDeleteRewardsApiIT.java
@@ -1,16 +1,28 @@
 package onlydust.com.marketplace.api.bootstrap.it.api;
 
+import onlydust.com.marketplace.accounting.domain.model.Invoice;
+import onlydust.com.marketplace.accounting.domain.model.RewardId;
+import onlydust.com.marketplace.accounting.domain.model.billingprofile.BillingProfile;
+import onlydust.com.marketplace.accounting.domain.model.billingprofile.PayoutInfo;
+import onlydust.com.marketplace.accounting.domain.model.user.UserId;
 import onlydust.com.marketplace.accounting.domain.service.BillingProfileService;
+import onlydust.com.marketplace.api.bootstrap.helper.UserAuthHelper;
+import onlydust.com.marketplace.api.postgres.adapter.entity.write.VerificationStatusEntity;
 import onlydust.com.marketplace.api.postgres.adapter.repository.ProjectRepository;
 import onlydust.com.marketplace.api.rest.api.adapter.authentication.AuthenticatedAppUserService;
-import onlydust.com.marketplace.project.domain.service.UserService;
+import onlydust.com.marketplace.kernel.model.bank.BankAccount;
+import onlydust.com.marketplace.kernel.model.blockchain.evm.ethereum.Name;
+import onlydust.com.marketplace.kernel.model.blockchain.evm.ethereum.WalletLocator;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import static onlydust.com.marketplace.api.rest.api.adapter.authentication.AuthenticationFilter.BEARER_PREFIX;
@@ -63,48 +75,48 @@ public class ProjectDeleteRewardsApiIT extends AbstractMarketplaceApiIT {
 
     @Autowired
     BillingProfileService billingProfileService;
-    @Autowired
-    UserService userService;
 
     @Test
     @Order(4)
     void should_prevent_cancel_reward_action_given_a_reward_already_in_an_invoice() throws IOException {
-        //TODO
-//        // Given
-//        final UserAuthHelper.AuthenticatedUser pierre = userAuthHelper.authenticatePierre();
-//        final String jwt = pierre.jwt();
-//        final UUID projectId = UUID.fromString("f39b827f-df73-498c-8853-99bc3f562723");
-//        final UUID userId = pierre.user().getId();
-//        final var rewardId = UUID.fromString("8fe07ae1-cf3b-4401-8958-a9e0b0aec7b0");
-//
-//        final OldCompanyBillingProfile companyBillingProfile = userService.getCompanyBillingProfile(userId);
-//        userService.updateBillingProfileType(userId, OldBillingProfileType.COMPANY);
-//        postgresOldBillingProfileAdapter.saveCompanyProfile(companyBillingProfile.toBuilder()
-//                .name(faker.rickAndMorty().character())
-//                .address(faker.address().fullAddress())
-//                .euVATNumber("111")
-//                .subjectToEuropeVAT(false)
-//                .oldCountry(OldCountry.fromIso3("FRA"))
-//                .usEntity(false)
-//                .status(OldVerificationStatus.VERIFIED)
-//                .build()
-//        );
-//        final BillingProfile.Id billingProfileId = BillingProfile.Id.of(companyBillingProfile.getId());
-//        billingProfileService.updateInvoiceMandateAcceptanceDate(UserId.of(userId), billingProfileId);
-//        final Invoice.Id invoiceId =
-//                billingProfileService.previewInvoice(UserId.of(userId), billingProfileId, List.of(RewardId.of(rewardId))).id();
-//        billingProfileService.uploadGeneratedInvoice(UserId.of(userId), billingProfileId, invoiceId,
-//                new FileSystemResource(Objects.requireNonNull(getClass().getResource("/invoices/invoice-sample.pdf")).getFile()).getInputStream());
-//
-//        // When
-//        client.delete()
-//                .uri(getApiURI(String.format(PROJECTS_REWARD, projectId, rewardId)))
-//                .header("Authorization", BEARER_PREFIX + jwt)
-//                // Then
-//                .exchange()
-//                .expectStatus()
-//                .isEqualTo(403)
-//                .expectBody()
-//                .jsonPath("$.message").isEqualTo("Cannot cancel reward %s which is already contained in an invoice".formatted(rewardId));
+        // Given
+        final UserAuthHelper.AuthenticatedUser pierre = userAuthHelper.authenticatePierre();
+        final String jwt = pierre.jwt();
+        final UUID projectId = UUID.fromString("f39b827f-df73-498c-8853-99bc3f562723");
+        final UUID userId = pierre.user().getId();
+        final var rewardId = UUID.fromString("8fe07ae1-cf3b-4401-8958-a9e0b0aec7b0");
+
+        final var billingProfile = billingProfileService.getBillingProfilesForUser(UserId.of(userId)).stream().findFirst().orElseThrow();
+        final BillingProfile.Id billingProfileId = billingProfile.getId();
+        accountingHelper.patchBillingProfile(billingProfileId.value(), null, VerificationStatusEntity.VERIFIED);
+        kybRepository.findByBillingProfileId(billingProfileId.value())
+                .ifPresent(kyb -> kybRepository.save(kyb.toBuilder()
+                        .country("FRA")
+                        .address("1 Infinite Loop, Cupertino, CA 95014, United States")
+                        .euVATNumber("FR12345678901")
+                        .name("Apple Inc.")
+                        .registrationDate(faker.date().birthday())
+                        .registrationNumber("123456789")
+                        .usEntity(false)
+                        .subjectToEuVAT(true)
+                        .verificationStatus(VerificationStatusEntity.VERIFIED).build()));
+        billingProfileService.updatePayoutInfo(billingProfileId, UserId.of(userId),
+                PayoutInfo.builder().ethWallet(new WalletLocator(new Name("foobar.eth")))
+                        .bankAccount(new BankAccount("BIC", "FR76000111222333334444")).build());
+        billingProfileService.updateInvoiceMandateAcceptanceDate(UserId.of(userId), billingProfileId);
+        final Invoice.Id invoiceId = billingProfileService.previewInvoice(UserId.of(userId), billingProfileId, List.of(RewardId.of(rewardId))).id();
+        billingProfileService.uploadGeneratedInvoice(UserId.of(userId), billingProfileId, invoiceId,
+                new FileSystemResource(Objects.requireNonNull(getClass().getResource("/invoices/invoice-sample.pdf")).getFile()).getInputStream());
+
+        // When
+        client.delete()
+                .uri(getApiURI(String.format(PROJECTS_REWARD, projectId, rewardId)))
+                .header("Authorization", BEARER_PREFIX + jwt)
+                // Then
+                .exchange()
+                .expectStatus()
+                .isForbidden()
+                .expectBody()
+                .jsonPath("$.message").isEqualTo("Reward %s cannot be cancelled because it is included in an invoice".formatted(rewardId));
     }
 }
