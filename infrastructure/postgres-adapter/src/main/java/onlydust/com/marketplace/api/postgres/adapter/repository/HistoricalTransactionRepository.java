0856f9327ed4a2d5f8b8739efb9dfd4073d7786b
Anthony Buisset <abuisset@gmail.com>
2024-03-21 18:00:27 +0100

    fix count query generation

--- infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/HistoricalTransactionRepository.java
+++ infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/HistoricalTransactionRepository.java
@@ -25,9 +25,9 @@ public interface HistoricalTransactionRepository extends JpaRepository<Historica
                 sa.sponsor_id = :sponsorId 
             UNION
             SELECT 
-                gen_random_uuid()   AS id,
-                abe.tech_created_at AS timestamp,
-                'ALLOCATION'        AS type,
+                gen_random_uuid()                                            AS id,
+                abe.tech_created_at                                          AS timestamp,
+                'ALLOCATION'                                                 AS type,
                 CAST(abe.payload #>> '{event, from, id}' AS UUID)            AS sponsor_account_id,
                 CAST(abe.payload #>> '{event, amount, value}' AS numeric)    AS amount,
                 accounting.usd_quote_at(ab.currency_id, abe.tech_created_at) AS usd_conversion_rate,
@@ -41,6 +41,25 @@ public interface HistoricalTransactionRepository extends JpaRepository<Historica
             JOIN accounting.account_books ab on ab.id = abe.account_book_id
             WHERE 
                 sa.sponsor_id = :sponsorId
-            """, nativeQuery = true)
+            """,
+            countQuery = """
+                    WITH deposits AS (SELECT sa.sponsor_id, count(*) as count
+                                      FROM accounting.sponsor_accounts sa
+                                               JOIN accounting.sponsor_account_transactions sat
+                                                    ON sat.account_id = sa.id AND sat.type = 'DEPOSIT'
+                                      GROUP BY sa.sponsor_id),
+                         spends as (SELECT sa.sponsor_id, count(*) as count
+                                    FROM accounting.sponsor_accounts sa
+                                             JOIN accounting.account_books_events abe
+                                                  ON CAST(abe.payload #>> '{event, from, id}' AS UUID) = sa.id AND
+                                                     abe.payload #>> '{event, @type}' = 'Transfer' AND
+                                                     abe.payload #>> '{event, to, type}' = 'PROJECT'
+                                             JOIN accounting.account_books ab on ab.id = abe.account_book_id
+                                    GROUP BY sa.sponsor_id)
+                    SELECT d.count + s.count
+                    FROM deposits d
+                             JOIN spends s ON d.sponsor_id = s.sponsor_id
+                    WHERE d.sponsor_id = :sponsorId
+                    """, nativeQuery = true)
     Page<HistoricalTransactionEntity> findAll(UUID sponsorId, Pageable pageable);
 }
