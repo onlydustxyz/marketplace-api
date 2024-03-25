ab46afff64bcc5bcf6acc006d50858130d420445
Anthony Buisset <abuisset@gmail.com>
2024-03-25 12:00:01 +0100

    fix migration

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000120_migrate_vat_in_snapshots.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000120_migrate_vat_in_snapshots.sql
@@ -2,23 +2,21 @@ update accounting.invoices
 set data = jsonb_set(data, '{billingProfileSnapshot, kybSnapshot, vatRegulationState}',
                      case
                          when data #>> '{billingProfileSnapshot, kybSnapshot, inEuropeanUnion}' = 'false'
-                             then 'NOT_APPLICABLE_NON_UE'
+                             then '"NOT_APPLICABLE_NON_UE"'::jsonb
                          when data #>> '{billingProfileSnapshot, kybSnapshot, isFrance}' = 'false'
-                             then 'REVERSE_CHARGE'
+                             then '"REVERSE_CHARGE"'::jsonb
                          when data #>> '{billingProfileSnapshot, kybSnapshot, subjectToEuVAT}' = 'false'
-                             then 'NOT_APPLICABLE_FRENCH_NOT_SUBJECT'
-                         else 'APPLICABLE'
+                             then '"NOT_APPLICABLE_FRENCH_NOT_SUBJECT"'::jsonb
+                         else '"APPLICABLE"'::jsonb
                          end, true)
-from accounting.invoices
 where data #> '{billingProfileSnapshot, kybSnapshot}' IS NOT NULL;
 
 update accounting.invoices
 set data = jsonb_set(data, '{billingProfileSnapshot, kybSnapshot, taxRate}',
                      case
                          when data #>> '{billingProfileSnapshot, kybSnapshot, vatRegulationState}' = 'APPLICABLE'
-                             then 0.2
-                         else 0
+                             then '0.2'::jsonb
+                         else '0'::jsonb
                          end, true)
-from accounting.invoices
 where data #> '{billingProfileSnapshot, kybSnapshot}' IS NOT NULL;
 
