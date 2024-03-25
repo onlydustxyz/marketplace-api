b675904474540c7d25f448ac653d213653b1afb6
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-03-25 18:27:04 +0100

    Refactor Payout preferences response

--- application/rest-api-adapter/src/main/java/onlydust/com/marketplace/api/rest/api/adapter/mapper/PayoutPreferenceMapper.java
+++ application/rest-api-adapter/src/main/java/onlydust/com/marketplace/api/rest/api/adapter/mapper/PayoutPreferenceMapper.java
@@ -3,6 +3,7 @@ package onlydust.com.marketplace.api.rest.api.adapter.mapper;
 import onlydust.com.marketplace.accounting.domain.view.PayoutPreferenceView;
 import onlydust.com.marketplace.accounting.domain.view.ShortBillingProfileView;
 import onlydust.com.marketplace.accounting.domain.view.ShortProjectView;
+import onlydust.com.marketplace.api.contract.model.PayoutPreferenceBillingProfileResponse;
 import onlydust.com.marketplace.api.contract.model.PayoutPreferencesItemResponse;
 import onlydust.com.marketplace.api.contract.model.ShortBillingProfileResponse;
 import onlydust.com.marketplace.api.contract.model.ShortProjectResponse;
@@ -14,8 +15,16 @@ public interface PayoutPreferenceMapper {
 
     static PayoutPreferencesItemResponse mapToResponse(final PayoutPreferenceView view) {
         return new PayoutPreferencesItemResponse()
-                .billingProfile(isNull(view.shortBillingProfileView()) ? null : billingProfileToShortResponse(view.shortBillingProfileView()))
+                .billingProfile(isNull(view.billingProfileView()) ? null : map(view.billingProfileView()))
                 .project(projectToResponse(view.shortProjectView()));
+    }
+
+    static PayoutPreferenceBillingProfileResponse map(final PayoutPreferenceView.BillingProfileView view) {
+        return new PayoutPreferenceBillingProfileResponse()
+                .id(view.id().value())
+                .name(view.name())
+                .enabled(view.enabled())
+                .type(BillingProfileMapper.map(view.type()));
     }
 
     static ShortBillingProfileResponse billingProfileToShortResponse(final ShortBillingProfileView view) {
