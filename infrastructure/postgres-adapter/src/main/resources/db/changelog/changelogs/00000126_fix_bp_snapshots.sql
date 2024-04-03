3b1d1e5556b20da18a1baafc672bd0068bbd7b22
Anthony Buisset <abuisset@gmail.com>
2024-04-03 08:45:48 +0200

    fix billing profile snapshots

---
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000126_fix_bp_snapshots.sql
@@ -0,0 +1,14 @@
+update accounting.invoices
+set data = jsonb_set(data, '{billingProfileSnapshot, kycSnapshot, usCitizen}', kyc.us_citizen::text::jsonb, true)
+from accounting.kyc
+where kyc.billing_profile_id = invoices.billing_profile_id
+  and data #> '{billingProfileSnapshot, kycSnapshot}' IS NOT NULL;
+
+update accounting.invoices
+set data = jsonb_set(data, '{billingProfileSnapshot, kybSnapshot, usEntity}', kyb.us_entity::text::jsonb, true)
+from accounting.kyb
+where kyb.billing_profile_id = invoices.billing_profile_id
+  and data #> '{billingProfileSnapshot, kybSnapshot}' IS NOT NULL;
+
+update accounting.invoices
+set data = jsonb_set(data, '{billingProfileSnapshot, id, uuid}', to_jsonb(billing_profile_id), true);
