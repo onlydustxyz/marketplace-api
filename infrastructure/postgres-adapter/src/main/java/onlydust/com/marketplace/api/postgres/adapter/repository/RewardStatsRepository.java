19bae18345660a49bd5e637a7f15be7a6d1b5baf
Olivier Fuxet <ofux@users.noreply.github.com>
2024-04-03 14:48:54 +0200

    Fix pending request count for BP members (#555)

--- infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/RewardStatsRepository.java
+++ infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/RewardStatsRepository.java
@@ -16,7 +16,9 @@ public interface RewardStatsRepository extends JpaRepository<RewardStatsEntity, 
                    COALESCE(SUM(r.amount), 0)                                                           AS processed_amount,
                    COALESCE(SUM(rsd.amount_usd_equivalent), 0)                                          AS processed_usd_amount,
                    COALESCE(SUM(r.amount) FILTER ( WHERE rs.status != 'COMPLETE' ), 0)                  AS pending_amount,
-                   COALESCE(COUNT(rs.status) FILTER ( WHERE rs.status = 'PENDING_REQUEST'), 0)                  AS pending_request_count,
+                   COALESCE(COUNT(rs.status) FILTER (
+                            WHERE rs.status = 'PENDING_REQUEST' AND rpu.role = 'ADMIN'
+                        ), 0)                                                                           AS pending_request_count,
                    COALESCE(SUM(rsd.amount_usd_equivalent) FILTER ( WHERE rs.status != 'COMPLETE' ), 0) AS pending_usd_amount,
                    JSONB_AGG(r.id)                                                                      AS reward_ids,
                    COALESCE(JSONB_AGG(wii.ids) FILTER ( WHERE wii.ids IS NOT NULL ), '[]')              AS reward_item_ids,
@@ -25,6 +27,8 @@ public interface RewardStatsRepository extends JpaRepository<RewardStatsEntity, 
                      JOIN accounting.reward_status_data rsd ON rsd.reward_id = r.id
                      JOIN accounting.reward_statuses rs ON rs.reward_id = r.id
                      LEFT JOIN reward_item_ids wii ON wii.reward_id = r.id
+                     LEFT JOIN iam.users caller ON caller.github_user_id = :contributorId
+                     LEFT JOIN accounting.billing_profiles_users rpu ON rpu.billing_profile_id = r.billing_profile_id AND rpu.user_id = caller.id
             WHERE (
                     (coalesce(:administratedBillingProfileIds) IS NULL AND r.recipient_id = :contributorId)
                     OR
