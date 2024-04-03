2c7c78ab9e5b1727b0651c8d5dca2e8f7ff006de
Anthony Buisset <abuisset@gmail.com>
2024-04-03 14:34:34 +0200

    add currency check

--- accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/view/TotalMoneyView.java
+++ accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/view/TotalMoneyView.java
@@ -2,8 +2,11 @@ package onlydust.com.marketplace.accounting.domain.view;
 
 import lombok.NonNull;
 import onlydust.com.marketplace.accounting.domain.model.Currency;
+import onlydust.com.marketplace.kernel.exception.OnlyDustException;
 
 import java.math.BigDecimal;
+
+import static onlydust.com.marketplace.kernel.exception.OnlyDustException.internalServerError;
 
 /***
  * Represents a total amount with its currency and the total equivalent in dollars.
@@ -12,7 +15,11 @@ import java.math.BigDecimal;
 public record TotalMoneyView(@NonNull BigDecimal amount,
                              @NonNull Currency currency,
                              BigDecimal dollarsEquivalent) {
-    public static TotalMoneyView add(TotalMoneyView left, TotalMoneyView right) {
+    public static TotalMoneyView add(TotalMoneyView left, @NonNull TotalMoneyView right) throws OnlyDustException {
+        if (left != null && !left.currency().equals(right.currency())) {
+            throw internalServerError("Cannot add amounts with different currencies");
+        }
+        
         return left == null ? right : new TotalMoneyView(
                 left.amount().add(right.amount()),
                 left.currency(),
