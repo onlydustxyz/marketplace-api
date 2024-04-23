a335eefb3f54ee533c2b50940d2670f52b8c5202
Anthony Buisset <abuisset@gmail.com>
2024-04-23 09:50:41 +0200

    validate ethereum account addresses

--- accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/service/PayoutInfoValidator.java
+++ accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/service/PayoutInfoValidator.java
@@ -3,6 +3,7 @@ package onlydust.com.marketplace.accounting.domain.service;
 import lombok.AllArgsConstructor;
 import onlydust.com.marketplace.accounting.domain.model.billingprofile.PayoutInfo;
 import onlydust.com.marketplace.accounting.domain.port.out.WalletValidator;
+import onlydust.com.marketplace.kernel.model.blockchain.evm.EvmAccountAddress;
 import onlydust.com.marketplace.kernel.model.blockchain.evm.ethereum.Name;
 import onlydust.com.marketplace.kernel.model.blockchain.evm.ethereum.WalletLocator;
 import onlydust.com.marketplace.kernel.model.blockchain.starknet.StarknetAccountAddress;
@@ -13,6 +14,7 @@ import static onlydust.com.marketplace.kernel.exception.OnlyDustException.badReq
 public class PayoutInfoValidator {
     private final WalletValidator<Name> ensValidator;
     private final WalletValidator<StarknetAccountAddress> starknetAccountAddressValidator;
+    private final WalletValidator<EvmAccountAddress> evmAccountAddressValidator;
 
     public void validate(PayoutInfo payoutInfo) {
         payoutInfo.ethWallet().ifPresent(this::validate);
@@ -21,11 +23,17 @@ public class PayoutInfoValidator {
 
     private void validate(WalletLocator wallet) {
         wallet.ens().ifPresent(this::validate);
+        wallet.accountAddress().ifPresent(this::validate);
     }
 
     private void validate(Name ens) {
         if (!ensValidator.isValid(ens))
             throw badRequest("%s is not a valid ENS".formatted(ens.toString()));
+    }
+
+    private void validate(EvmAccountAddress address) {
+        if (!evmAccountAddressValidator.isValid(address))
+            throw badRequest("%s is not a valid Ethereum account address".formatted(address.toString()));
     }
 
     private void validate(StarknetAccountAddress address) {
--- bootstrap/src/main/java/onlydust/com/marketplace/api/bootstrap/configuration/AccountingConfiguration.java
+++ bootstrap/src/main/java/onlydust/com/marketplace/api/bootstrap/configuration/AccountingConfiguration.java
@@ -7,6 +7,7 @@ import onlydust.com.marketplace.accounting.domain.port.in.*;
 import onlydust.com.marketplace.accounting.domain.port.out.*;
 import onlydust.com.marketplace.accounting.domain.service.*;
 import onlydust.com.marketplace.api.infura.adapters.EthInfuraEnsValidatorAdapter;
+import onlydust.com.marketplace.api.infura.adapters.InfuraEvmAccountAddressValidatorAdapter;
 import onlydust.com.marketplace.api.infura.adapters.StarknetInfuraAccountValidatorAdapter;
 import onlydust.com.marketplace.api.sumsub.webhook.adapter.mapper.SumsubMapper;
 import onlydust.com.marketplace.kernel.port.output.*;
@@ -66,8 +67,9 @@ public class AccountingConfiguration {
 
     @Bean
     public PayoutInfoValidator payoutInfoValidator(final @NonNull EthInfuraEnsValidatorAdapter ethereumEnsValidatorAdapter,
-                                                   final @NonNull StarknetInfuraAccountValidatorAdapter starknetEnsValidatorAdapter) {
-        return new PayoutInfoValidator(ethereumEnsValidatorAdapter, starknetEnsValidatorAdapter);
+                                                   final @NonNull StarknetInfuraAccountValidatorAdapter starknetEnsValidatorAdapter,
+                                                   final @NonNull InfuraEvmAccountAddressValidatorAdapter infuraEvmAccountAddressValidatorAdapter) {
+        return new PayoutInfoValidator(ethereumEnsValidatorAdapter, starknetEnsValidatorAdapter, infuraEvmAccountAddressValidatorAdapter);
     }
 
     @Bean
