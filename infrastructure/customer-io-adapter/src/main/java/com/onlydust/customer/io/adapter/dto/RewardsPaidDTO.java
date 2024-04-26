bf3c36eeed4c83dc4b8dda15649e5748dc94f3fd
PierreOucif <pierre.oucif@gadz.org>
2024-04-26 10:45:12 +0200

    Fix rewards paid email

--- infrastructure/customer-io-adapter/src/main/java/com/onlydust/customer/io/adapter/dto/RewardsPaidDTO.java
+++ infrastructure/customer-io-adapter/src/main/java/com/onlydust/customer/io/adapter/dto/RewardsPaidDTO.java
@@ -3,7 +3,7 @@ package com.onlydust.customer.io.adapter.dto;
 import lombok.NonNull;
 import onlydust.com.marketplace.accounting.domain.events.RewardsPaid;
 
-public record RewardsPaidDTO(@NonNull String rewardNames, @NonNull String username) {
+public record RewardsPaidDTO(@NonNull String rewardsDetails, @NonNull String username) {
 
     public static RewardsPaidDTO fromEvent(RewardsPaid rewardsPaid) {
         return new RewardsPaidDTO(MailDTO.getRewardNames(rewardsPaid.shortRewards()), rewardsPaid.recipientGithubLogin());
