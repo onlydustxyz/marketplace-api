7bac0b2ff2340f57d754965a27b83c4d2469d1ba
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-05-22 11:50:54 +0200

    Add IT

--- bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/CommitteeApiIT.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/CommitteeApiIT.java
@@ -178,7 +178,65 @@ public class CommitteeApiIT extends AbstractMarketplaceApiIT {
                               }
                             }
                         }
-                          """);
+                        """);
+    }
+
+    @Test
+    @Order(3)
+    void should_get_application_no_matter_the_status() {
+        // Given
+        final UserAuthHelper.AuthenticatedUser pierre = userAuthHelper.authenticatePierre();
+        committeeFacadePort.updateStatus(committeeId, Committee.Status.OPEN_TO_VOTES);
+
+        // When
+        client.get()
+                .uri(getApiURI(COMMITTEES_APPLICATIONS.formatted(committeeId), Map.of("projectId", bretzel.toString())))
+                .header("Authorization", "Bearer " + pierre.jwt())
+                // Then
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody()
+                .json("""
+                          {
+                           "projectInfos": {
+                              "id": "7d04163c-4187-4313-8066-61504d34fc56",
+                              "slug": "bretzel",
+                              "name": "Bretzel",
+                              "logoUrl": "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/5003677688814069549.png",
+                              "projectLeads": [
+                                {
+                                  "githubUserId": 98735421,
+                                  "login": "pacovilletard",
+                                  "avatarUrl": "https://avatars.githubusercontent.com/u/98735421?v=4",
+                                  "id": "f20e6812-8de8-432b-9c31-2920434fe7d0"
+                                },
+                                {
+                                  "githubUserId": 8642470,
+                                  "login": "gregcha",
+                                  "avatarUrl": "https://avatars.githubusercontent.com/u/8642470?v=4",
+                                  "id": "45e98bf6-25c2-4edf-94da-e340daba8964"
+                                },
+                                {
+                                  "githubUserId": 16590657,
+                                  "login": "PierreOucif",
+                                  "avatarUrl": "https://avatars.githubusercontent.com/u/16590657?v=4",
+                                  "id": "fc92397c-3431-4a84-8054-845376b630a0"
+                                }
+                              ],
+                              "longDescription": "[Bretzel](http://bretzel.club/) is your best chance to match with your secret crush      \\nEver liked someone but never dared to tell them?      \\n      \\n**Bretzel** is your chance to match with your secret crush      \\nAll you need is a LinkedIn profile.      \\n      \\n1. **Turn LinkedIn into a bretzel party:** Switch the bretzel mode ON — you'll see bretzels next to everyone. Switch it OFF anytime.      \\n2. **Give your bretzels under the radar:** Give a bretzel to your crush, they will never know about it, unless they give you a bretzel too. Maybe they already have?      \\n3. **Ooh la la, it's a match!**  You just got bretzel’d! See all your matches in a dedicated space, and start chatting!",
+                              "shortDescription": "A project for people who love fruits",
+                              "last3monthsMetrics": {
+                                "activeContributors": 0,
+                                "newContributors": 0,
+                                "contributorsRewarded": 0,
+                                "openIssues": 0,
+                                "contributionsCompleted": 0,
+                                "amountSentInUsd": 0.0
+                              }
+                            }
+                        }
+                        """);
     }
 
 
