f3472a5bdb8b73f64f30614c4b16a4f9bd8fdd33
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-05-27 17:52:44 +0200

    Fix IT by ordering answers

--- infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/CommitteeProjectAnswerViewRepository.java
+++ infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/CommitteeProjectAnswerViewRepository.java
@@ -2,11 +2,23 @@ package onlydust.com.marketplace.api.postgres.adapter.repository;
 
 import onlydust.com.marketplace.api.postgres.adapter.entity.read.CommitteeProjectAnswerViewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface CommitteeProjectAnswerViewRepository extends JpaRepository<CommitteeProjectAnswerViewEntity, CommitteeProjectAnswerViewEntity.PrimaryKey> {
 
+    @Query(value = """
+            select
+                a
+            from
+                CommitteeProjectAnswerViewEntity a
+                join fetch a.projectQuestion
+            where
+                a.committeeId = :committeeId
+                and a.projectId = :projectId
+            order by a.projectQuestion.rank
+            """)
     List<CommitteeProjectAnswerViewEntity> findByCommitteeIdAndAndProjectId(UUID committeeId, UUID projectId);
 }
