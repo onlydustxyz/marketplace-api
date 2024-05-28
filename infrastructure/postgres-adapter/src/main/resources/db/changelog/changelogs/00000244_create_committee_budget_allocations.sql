e2b75400fb2231b20b2554ca11d6a9f50decc27d
Anthony Buisset <abuisset@gmail.com>
2024-05-28 22:02:11 +0200

    remove committee budgets tables

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000244_create_committee_budget_allocations.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000244_create_committee_budget_allocations.sql
@@ -1,20 +1,3 @@
-create table committee_budgets
-(
-    committee_id           uuid primary key references committees (id),
-    currency_id            uuid not null references currencies (id),
-    total_amount           numeric,
-    min_project_allocation numeric,
-    max_project_allocation numeric,
-    tech_created_at        timestamp with time zone default now(),
-    tech_updated_at        timestamp with time zone default now()
-);
-
-create trigger committee_budgets_tech_updated_at
-    before update
-    on committee_budgets
-    for each row
-execute function set_tech_updated_at();
-
 create table committee_budget_allocations
 (
     committee_id    uuid    not null references committees (id),
