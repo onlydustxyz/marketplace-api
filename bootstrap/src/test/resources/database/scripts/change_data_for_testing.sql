fac883a5ed5e20999e1279c970bb9d79ad70cdbc
Anthony Buisset <abuisset@gmail.com>
2024-05-30 11:25:14 +0200

    refresh materialized views when loading dump

--- bootstrap/src/test/resources/database/scripts/change_data_for_testing.sql
+++ bootstrap/src/test/resources/database/scripts/change_data_for_testing.sql
@@ -9,20 +9,18 @@ create table if not exists auth.user_providers
     id uuid primary key
 );
 
+REFRESH MATERIALIZED VIEW contributions_stats_per_user;
+REFRESH MATERIALIZED VIEW contributions_stats_per_user_per_week;
+REFRESH MATERIALIZED VIEW contributions_stats_per_ecosystem_per_user;
+REFRESH MATERIALIZED VIEW contributions_stats_per_ecosystem_per_user_per_week;
+REFRESH MATERIALIZED VIEW contributions_stats_per_language_per_user;
+REFRESH MATERIALIZED VIEW received_rewards_stats_per_user;
+REFRESH MATERIALIZED VIEW received_rewards_stats_per_user_per_week;
+REFRESH MATERIALIZED VIEW received_rewards_stats_per_ecosystem_per_user;
+REFRESH MATERIALIZED VIEW received_rewards_stats_per_ecosystem_per_user_per_week;
+REFRESH MATERIALIZED VIEW received_rewards_stats_per_language_per_user;
+REFRESH MATERIALIZED VIEW received_rewards_stats_per_project_per_user;
 REFRESH MATERIALIZED VIEW global_users_ranks;
-
-insert into languages (id, name)
-values ('ca600cac-0f45-44e9-a6e8-25e21b0c6887', 'Rust'),
-       ('75ce6b37-8610-4600-8d2d-753b50aeda1e', 'Typescript'),
-       ('f57d0866-89f3-4613-aaa2-32f4f4ecc972', 'Cairo'),
-       ('d69b6d3e-f583-4c98-92d0-99a56f6f884a', 'Solidity');
-
-insert into public.language_file_extensions (extension, language_id)
-values ('rs', 'ca600cac-0f45-44e9-a6e8-25e21b0c6887'),
-       ('tsx', '75ce6b37-8610-4600-8d2d-753b50aeda1e'),
-       ('ts', '75ce6b37-8610-4600-8d2d-753b50aeda1e'),
-       ('cairo', 'f57d0866-89f3-4613-aaa2-32f4f4ecc972'),
-       ('sol', 'd69b6d3e-f583-4c98-92d0-99a56f6f884a');
 
 insert into ecosystem_banners (id, font_color, image_url)
 values ('12ae34bb-dfbc-48ed-bef0-08c5c087593d', 'DARK', 'https://s3.amazonaws.com/onlydust/ecosystem_banners/zama-md.png'),
