377d9489744817d8a462e6055aed6b60cec58759
PierreOucif <pierre.oucif@gadz.org>
2024-05-30 11:21:34 +0200

    Add unique index on projects_languages view

--- bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/CronIT.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/bootstrap/it/api/CronIT.java
@@ -1,5 +1,6 @@
 package onlydust.com.marketplace.api.bootstrap.it.api;
 
+import onlydust.com.marketplace.project.domain.port.input.LanguageFacadePort;
 import onlydust.com.marketplace.project.domain.port.input.UserFacadePort;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,13 @@ public class CronIT extends AbstractMarketplaceApiIT {
 
     @Autowired
     UserFacadePort userFacadePort;
+    @Autowired
+    LanguageFacadePort languageFacadePort;
 
     @Test
     void should_be_able_to_refresh_materialized_views() {
         assertDoesNotThrow(() -> userFacadePort.refreshUserRanksAndStats());
+        assertDoesNotThrow(() -> languageFacadePort.updateProjectsLanguages());
     }
 
     @Test
--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000246_create_project_languages_materialized_view.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000246_create_project_languages_materialized_view.sql
@@ -3,3 +3,6 @@ select distinct pgr.project_id, lfe.language_id
 from public_contributions c
          join project_github_repos pgr on pgr.github_repo_id = c.repo_id
          join language_file_extensions lfe ON lfe.extension = ANY (c.main_file_extensions);
+
+CREATE UNIQUE INDEX project_languages_project_id_language_id_uindex
+    ON project_languages (project_id, language_id);
