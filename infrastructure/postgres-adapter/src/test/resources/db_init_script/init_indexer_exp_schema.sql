90fcca0e61b37f74dd1f202a351a925869d62c7d
Anthony Buisset <abuisset@gmail.com>
2024-05-31 15:10:35 +0200

    fix db init script

--- infrastructure/postgres-adapter/src/test/resources/db_init_script/init_indexer_exp_schema.sql
+++ infrastructure/postgres-adapter/src/test/resources/db_init_script/init_indexer_exp_schema.sql
@@ -124,6 +124,40 @@ create table indexer_exp.github_issues_assignees
     primary key (issue_id, user_id)
 );
 
+CREATE TABLE indexer_exp.github_labels
+(
+    id              BIGINT PRIMARY KEY,
+    name            TEXT      NOT NULL,
+    description     TEXT,
+    tech_created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    tech_updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER indexer_exp_github_labels_set_tech_updated_at
+    BEFORE UPDATE
+    ON
+        indexer_exp.github_labels
+    FOR EACH ROW
+EXECUTE PROCEDURE set_tech_updated_at();
+
+CREATE TABLE indexer_exp.github_issues_labels
+(
+    issue_id        BIGINT    NOT NULL REFERENCES indexer_exp.github_issues (id),
+    label_id        BIGINT    NOT NULL REFERENCES indexer_exp.github_labels (id),
+    tech_created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    tech_updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (issue_id, label_id)
+);
+
+CREATE TRIGGER indexer_exp_github_issues_labels_set_tech_updated_at
+    BEFORE UPDATE
+    ON
+        indexer_exp.github_issues_labels
+    FOR EACH ROW
+EXECUTE PROCEDURE set_tech_updated_at();
+
+
+
 create table indexer_exp.github_pull_requests
 (
     id                   bigint                                       not null
