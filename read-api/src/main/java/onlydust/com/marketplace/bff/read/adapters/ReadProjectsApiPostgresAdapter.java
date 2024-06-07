eacc83dc4b0020da055260ff598486d57e56d6be
PierreOucif <pierre.oucif@gadz.org>
2024-06-07 09:26:30 +0200

    Fix ITs #4

--- read-api/src/main/java/onlydust/com/marketplace/bff/read/adapters/ReadProjectsApiPostgresAdapter.java
+++ read-api/src/main/java/onlydust/com/marketplace/bff/read/adapters/ReadProjectsApiPostgresAdapter.java
@@ -36,7 +36,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
