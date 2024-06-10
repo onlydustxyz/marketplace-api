62ac4f7db09c537d0ae9a55189ebb4b5366837e7
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-06-10 10:43:22 +0200

    Fix LazyInitializationException in SlackApiAdapter when applying

--- project-domain/src/main/java/onlydust/com/marketplace/project/domain/service/UserService.java
+++ project-domain/src/main/java/onlydust/com/marketplace/project/domain/service/UserService.java
@@ -5,17 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import onlydust.com.marketplace.kernel.exception.OnlyDustException;
 import onlydust.com.marketplace.kernel.model.AuthenticatedUser;
 import onlydust.com.marketplace.kernel.pagination.Page;
-import onlydust.com.marketplace.kernel.pagination.SortDirection;
 import onlydust.com.marketplace.kernel.port.output.ImageStoragePort;
 import onlydust.com.marketplace.project.domain.gateway.DateProvider;
-import onlydust.com.marketplace.project.domain.model.*;
+import onlydust.com.marketplace.project.domain.model.GithubMembership;
+import onlydust.com.marketplace.project.domain.model.GithubUserIdentity;
+import onlydust.com.marketplace.project.domain.model.User;
+import onlydust.com.marketplace.project.domain.model.UserProfile;
 import onlydust.com.marketplace.project.domain.port.input.ProjectObserverPort;
 import onlydust.com.marketplace.project.domain.port.input.UserFacadePort;
 import onlydust.com.marketplace.project.domain.port.input.UserObserverPort;
 import onlydust.com.marketplace.project.domain.port.output.GithubSearchPort;
 import onlydust.com.marketplace.project.domain.port.output.ProjectStoragePort;
 import onlydust.com.marketplace.project.domain.port.output.UserStoragePort;
-import onlydust.com.marketplace.project.domain.view.*;
+import onlydust.com.marketplace.project.domain.view.ProjectOrganizationView;
+import onlydust.com.marketplace.project.domain.view.RewardDetailsView;
+import onlydust.com.marketplace.project.domain.view.RewardItemView;
+import onlydust.com.marketplace.project.domain.view.UserProfileView;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.InputStream;
@@ -76,12 +81,14 @@ public class UserService implements UserFacadePort {
     }
 
     @Override
+    @Transactional
     public UserProfileView updateProfile(UUID userId, UserProfile userProfile) {
         userStoragePort.saveProfile(userId, userProfile);
         return userStoragePort.getProfileById(userId);
     }
 
     @Override
+    @Transactional
     public void refreshActiveUserProfiles(ZonedDateTime since) {
         final var activeUsers = userStoragePort.getUsersLastSeenSince(since);
 
@@ -106,6 +113,7 @@ public class UserService implements UserFacadePort {
     }
 
     @Override
+    @Transactional
     public void updateGithubProfile(User user) {
         userStoragePort.saveUser(githubSearchPort.getUserProfile(user.getGithubUserId())
                 .map(githubUserProfile -> User.builder()
@@ -118,42 +126,49 @@ public class UserService implements UserFacadePort {
     }
 
     @Override
+    @Transactional
     public void refreshUserRanksAndStats() {
         userStoragePort.refreshUserRanksAndStats();
     }
 
     @Override
+    @Transactional
     public void historizeUserRanks() {
         userStoragePort.historizeUserRanks();
     }
 
     @Override
+    @Transactional
     public void markUserAsOnboarded(UUID userId) {
         userStoragePort.updateOnboardingWizardDisplayDate(userId, dateProvider.now());
     }
 
     @Override
+    @Transactional
     public void updateTermsAndConditionsAcceptanceDate(UUID userId) {
         userStoragePort.updateTermsAndConditionsAcceptanceDate(userId, dateProvider.now());
     }
 
     @Override
+    @Transactional
     public void acceptInvitationToLeadProject(Long githubUserId, UUID projectId) {
         userStoragePort.acceptProjectLeaderInvitation(githubUserId, projectId);
     }
 
     @Override
+    @Transactional
     public void applyOnProject(UUID userId, UUID projectId) {
         final var applicationId = userStoragePort.createApplicationOnProject(userId, projectId);
         projectObserverPort.onUserApplied(projectId, userId, applicationId);
     }
 
     @Override
+    @Transactional
     public RewardDetailsView getRewardByIdForRecipientIdAndAdministratedBillingProfileIds(UUID rewardId, Long recipientId,
                                                                                           List<UUID> companyAdminBillingProfileIds) {
         final RewardDetailsView reward = userStoragePort.findRewardById(rewardId);
         if (!reward.getTo().getGithubUserId().equals(recipientId) &&
-            (isNull(reward.getBillingProfileId()) || !companyAdminBillingProfileIds.contains(reward.getBillingProfileId()))) {
+                (isNull(reward.getBillingProfileId()) || !companyAdminBillingProfileIds.contains(reward.getBillingProfileId()))) {
             throw OnlyDustException.forbidden("Only recipient user or billing profile admin linked to this reward can read its details");
         }
         return reward;
@@ -163,14 +178,15 @@ public class UserService implements UserFacadePort {
                                                                                                       int pageSize, List<UUID> companyAdminBillingProfileIds) {
         final Page<RewardItemView> page = userStoragePort.findRewardItemsPageById(rewardId, pageIndex, pageSize);
         if (page.getContent().stream().anyMatch(rewardItemView -> !rewardItemView.getRecipientId().equals(recipientId)) &&
-            page.getContent().stream().anyMatch(rewardItemView -> isNull(rewardItemView.getBillingProfileId())
-                                                                  || !companyAdminBillingProfileIds.contains(rewardItemView.getBillingProfileId()))) {
+                page.getContent().stream().anyMatch(rewardItemView -> isNull(rewardItemView.getBillingProfileId())
+                        || !companyAdminBillingProfileIds.contains(rewardItemView.getBillingProfileId()))) {
             throw OnlyDustException.forbidden("Only recipient user or billing profile admin linked to this reward can read its details");
         }
         return page;
     }
 
     @Override
+    @Transactional
     public void claimProjectForAuthenticatedUser(UUID projectId, User user) {
         final var projectLeaders = projectStoragePort.getProjectLeadIds(projectId);
         final var projectInvitedLeaders = projectStoragePort.getProjectInvitedLeadIds(projectId);
@@ -187,14 +203,15 @@ public class UserService implements UserFacadePort {
                 .anyMatch(org -> cannotBeClaimedByUser(user, org));
         if (isNotClaimable) {
             throw OnlyDustException.forbidden("User must be github admin on every organizations not installed and at " +
-                                              "least member on every organization already installed linked to the " +
-                                              "project");
+                    "least member on every organization already installed linked to the " +
+                    "project");
 
         }
         userStoragePort.saveProjectLead(user.getId(), projectId);
     }
 
     @Override
+    @Transactional
     public URL saveAvatarImage(InputStream imageInputStream) {
         return this.imageStoragePort.storeImage(imageInputStream);
     }
