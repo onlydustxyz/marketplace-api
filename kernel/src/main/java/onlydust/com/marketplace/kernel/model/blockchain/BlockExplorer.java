bd8afebee0f75d5afe7a9e812aeb5e4f192b8f55
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-06-10 18:12:43 +0200

    [B-1885] Import github package in marketplace-api (#786)
    * migrate to marketplace-kernel package
    
    * configure maven in CI
    
    remove actor

--- kernel/src/main/java/onlydust/com/marketplace/kernel/model/blockchain/BlockExplorer.java
+++
@@ -1,7 +0,0 @@
-package onlydust.com.marketplace.kernel.model.blockchain;
-
-import java.net.URI;
-
-public interface BlockExplorer<TransactionHash> {
-    URI url(TransactionHash hash);
-}
--- kernel/src/main/java/onlydust/com/marketplace/kernel/pagination/SortDirection.java
+++
@@ -1,5 +0,0 @@
-package onlydust.com.marketplace.kernel.pagination;
-
-public enum SortDirection {
-    asc, desc
-}
