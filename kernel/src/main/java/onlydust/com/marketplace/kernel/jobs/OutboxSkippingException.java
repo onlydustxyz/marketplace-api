9399883940638cf2f6a8b6534b6c3c949cf47be2
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-06-10 18:12:43 +0200

    [B-1885] Import github package in marketplace-api (#786)
    * migrate to marketplace-kernel package
    
    * configure maven in CI
    
    remove actor
    
    add username

--- kernel/src/main/java/onlydust/com/marketplace/kernel/jobs/OutboxSkippingException.java
+++
@@ -1,7 +0,0 @@
-package onlydust.com.marketplace.kernel.jobs;
-
-public class OutboxSkippingException extends RuntimeException {
-    public OutboxSkippingException(final String message) {
-        super(message);
-    }
-}
--- kernel/src/main/java/onlydust/com/marketplace/kernel/visitor/Visitable.java
+++
@@ -1,5 +0,0 @@
-package onlydust.com.marketplace.kernel.visitor;
-
-public interface Visitable<T extends Visitable<T>> {
-    <R> R accept(Visitor<T, R> visitor);
-}
