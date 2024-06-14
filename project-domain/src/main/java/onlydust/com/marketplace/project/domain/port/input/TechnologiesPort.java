fdf9930df2274f640fc43028755d8a2283b3c3c2
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-06-14 13:36:05 +0200

    remove suggest technology endpoint (#804)

--- project-domain/src/main/java/onlydust/com/marketplace/project/domain/port/input/TechnologiesPort.java
+++ project-domain/src/main/java/onlydust/com/marketplace/project/domain/port/input/TechnologiesPort.java
@@ -1,11 +1,7 @@
 package onlydust.com.marketplace.project.domain.port.input;
 
-import onlydust.com.marketplace.project.domain.model.User;
-
 import java.util.List;
 
 public interface TechnologiesPort {
-    void suggest(String name, User requester);
-
     List<String> getAllUsedTechnologies();
 }
