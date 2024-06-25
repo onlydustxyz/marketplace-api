8cd3510e7cbaf13e2f868c900845bc90fde989ec
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-06-25 15:56:51 +0200

    Fix view

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000274_add_project_member_view.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000274_add_project_member_view.sql
@@ -1,7 +1,12 @@
 CREATE VIEW project_members AS
-SELECT coalesce(pc.project_id, pl.project_id)        as project_id,
-       coalesce(pc.github_user_id, u.github_user_id) as github_user_id,
-       u.id                                          as user_id
+SELECT pc.project_id     as project_id,
+       pc.github_user_id as github_user_id,
+       u.id              as user_id
 FROM projects_contributors pc
          LEFT JOIN iam.users u on u.github_user_id = pc.github_user_id
-         FULL JOIN project_leads pl on pl.project_id = pc.project_id and pl.user_id = u.id;
+UNION
+SELECT pl.project_id    as project_id,
+       u.github_user_id as github_user_id,
+       pl.user_id       as user_id
+FROM project_leads pl
+         LEFT JOIN iam.users u on u.id = pl.user_id;
