5551aef01dc0519511a0fc8fe38d27d928fb69b5
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-06-25 15:33:00 +0200

    Fix tests

--- read-api/src/main/java/onlydust/com/marketplace/api/read/repositories/GithubIssueReadRepository.java
+++ read-api/src/main/java/onlydust/com/marketplace/api/read/repositories/GithubIssueReadRepository.java
@@ -13,8 +13,6 @@ public interface GithubIssueReadRepository extends Repository<GithubIssueReadEnt
             SELECT i
             FROM GithubIssueReadEntity i
             JOIN FETCH i.goodFirstIssueOf p
-            JOIN FETCH i.applications
-            JOIN FETCH i.labels
             JOIN FETCH i.author
             JOIN FETCH i.repo
             WHERE p.id = :projectId
