d81bae8585f0698aa91c6a73d122abc74b1565e3
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-06-26 09:23:37 +0200

    update LLM prompt (#856)

--- infrastructure/langchain/src/main/java/onlydust/com/marketplace/api/infrastructure/langchain/adapters/LangchainLLMAdapter.java
+++ infrastructure/langchain/src/main/java/onlydust/com/marketplace/api/infrastructure/langchain/adapters/LangchainLLMAdapter.java
@@ -1,17 +1,24 @@
 package onlydust.com.marketplace.api.infrastructure.langchain.adapters;
 
-import dev.langchain4j.service.UserMessage;
-import dev.langchain4j.service.V;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.spring.AiService;
 import onlydust.com.marketplace.project.domain.port.output.LLMPort;
 
 @AiService
 public interface LangchainLLMAdapter extends LLMPort {
     @Override
-    @UserMessage("""
-            Based on the following issue comment taken from GitHub:
-            "{{comment}}"
-            Does it express an interest from the author in helping to resolve the issue?
+    @SystemMessage("""
+            You are a classifier assistant.
+            You classify if a comment on a github issue is a demand for taking the issue.
+            If the user state that he can work on it you have to include him as well.
+            If the commenter suggest someone else you don't have to include him.
+            Some messages only with an emoji hand or raised hand like 👋 can symbolize interest to take the issue, consider them TRUE.
+            Be aware that some comments talk about other things than the issue.
+            We only want to match contributors who want to take charge of the whole issue.
+            Here are some examples of candidature comments:
+               - "I want to take it"
+               - "On it"
+               - "Can I take this one?
             """)
-    boolean isCommentShowingInterestToContribute(@V("comment") String comment);
+    boolean isCommentShowingInterestToContribute(String comment);
 }
