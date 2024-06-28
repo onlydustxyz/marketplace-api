6ed770512cb0578e8d689e45b7f5087e222530d6
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-06-28 14:43:20 +0200

    add installation status in get issue by id (#868)

--- read-api/src/main/java/onlydust/com/marketplace/api/read/adapters/ReadIssuesApiPostgresAdapter.java
+++ read-api/src/main/java/onlydust/com/marketplace/api/read/adapters/ReadIssuesApiPostgresAdapter.java
@@ -2,7 +2,7 @@ package onlydust.com.marketplace.api.read.adapters;
 
 import lombok.AllArgsConstructor;
 import onlydust.com.marketplace.api.contract.ReadIssuesApi;
-import onlydust.com.marketplace.api.contract.model.GithubIssueLinkResponse;
+import onlydust.com.marketplace.api.contract.model.GithubIssueResponse;
 import onlydust.com.marketplace.api.read.entities.github.GithubIssueReadEntity;
 import onlydust.com.marketplace.api.read.repositories.GithubIssueReadRepository;
 import org.springframework.context.annotation.Profile;
@@ -21,8 +21,8 @@ public class ReadIssuesApiPostgresAdapter implements ReadIssuesApi {
     private final GithubIssueReadRepository githubIssueReadRepository;
 
     @Override
-    public ResponseEntity<GithubIssueLinkResponse> getIssue(Long issueId) {
+    public ResponseEntity<GithubIssueResponse> getIssue(Long issueId) {
         final var issue = githubIssueReadRepository.findById(issueId);
-        return ok(issue.map(GithubIssueReadEntity::toLinkDto).orElseThrow(() -> notFound("Issue %s not found".formatted(issueId))));
+        return ok(issue.map(GithubIssueReadEntity::toDto).orElseThrow(() -> notFound("Issue %s not found".formatted(issueId))));
     }
 }
