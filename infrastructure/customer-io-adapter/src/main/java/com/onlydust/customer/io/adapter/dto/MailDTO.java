52a8a21831976768d93e4b782cd914179ec16b53
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-05 14:30:02 +0200

    Send emails (via customer-io) by consuming outbox-pattern events

--- infrastructure/customer-io-adapter/src/main/java/com/onlydust/customer/io/adapter/dto/MailDTO.java
+++ infrastructure/customer-io-adapter/src/main/java/com/onlydust/customer/io/adapter/dto/MailDTO.java
@@ -7,6 +7,7 @@ import lombok.NonNull;
 import onlydust.com.marketplace.accounting.domain.events.*;
 import onlydust.com.marketplace.accounting.domain.events.dto.ShortReward;
 import onlydust.com.marketplace.project.domain.model.event.NewCommitteeApplication;
+import onlydust.com.marketplace.project.domain.model.event.ProjectApplicationsToReviewByUser;
 
 import java.math.RoundingMode;
 import java.util.List;
@@ -79,6 +80,16 @@ public record MailDTO<MessageData>(@NonNull @JsonProperty("transactional_message
                 newCommitteeApplication.getUserId()), customerIOProperties.getOnlyDustMarketingEmail(), newCommitteeApplication.getEmail(),
                 "Your application to committee %s".formatted(newCommitteeApplication.getCommitteeName()),
                 NewCommitteeApplicationDTO.fromEvent(newCommitteeApplication));
+    }
+
+    public static MailDTO<ProjectApplicationsToReviewByUserDTO> fromProjectApplicationsToReviewByUser(@NonNull CustomerIOProperties customerIOProperties,
+                                                                                                      @NonNull ProjectApplicationsToReviewByUser projectApplicationsToReviewByUser) {
+        return new MailDTO<>(customerIOProperties.getProjectApplicationsToReviewByUserEmailId().toString(),
+                mapIdentifiers(projectApplicationsToReviewByUser.getEmail(), projectApplicationsToReviewByUser.getUserId()),
+                customerIOProperties.getOnlyDustMarketingEmail(),
+                projectApplicationsToReviewByUser.getEmail(),
+                "Applications to review daily report",
+                ProjectApplicationsToReviewByUserDTO.fromEvent(projectApplicationsToReviewByUser));
     }
 
     private static IdentifiersDTO mapIdentifiers(@NonNull String email, UUID id) {
