2afb0f9180933fe7de28e75422110e1639591697
Anthony Buisset <abuisset@gmail.com>
2024-07-09 18:43:39 +0200

    code cleanup

--- accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/port/out/SponsorAccountStorage.java
+++ accounting-domain/src/main/java/onlydust/com/marketplace/accounting/domain/port/out/SponsorAccountStorage.java
@@ -1,12 +1,8 @@
 package onlydust.com.marketplace.accounting.domain.port.out;
 
-import lombok.NonNull;
 import onlydust.com.marketplace.accounting.domain.model.Currency;
-import onlydust.com.marketplace.accounting.domain.model.HistoricalTransaction;
 import onlydust.com.marketplace.accounting.domain.model.SponsorAccount;
 import onlydust.com.marketplace.accounting.domain.model.SponsorId;
-import onlydust.com.marketplace.kernel.pagination.Page;
-import onlydust.com.marketplace.kernel.pagination.SortDirection;
 
 import java.util.List;
 
@@ -16,13 +12,6 @@ public interface SponsorAccountStorage extends SponsorAccountProvider {
     void delete(SponsorAccount.Id sponsorAccountId, SponsorAccount.Transaction.Id transactionId);
 
     List<SponsorAccount> getSponsorAccounts(SponsorId sponsorId);
-
-    Page<HistoricalTransaction> transactionsOf(@NonNull SponsorId sponsorId,
-                                               @NonNull HistoricalTransaction.Filters filters,
-                                               @NonNull Integer pageIndex,
-                                               @NonNull Integer pageSize,
-                                               @NonNull HistoricalTransaction.Sort sort,
-                                               @NonNull SortDirection direction);
 
     List<SponsorAccount> find(SponsorId sponsorId, Currency.Id currencyId);
 }
