6a033e6328e4f0585d01866b86861f845b1eb316
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-09 17:32:35 +0200

    IT

--- read-api/src/main/java/onlydust/com/marketplace/api/read/entities/billing_profile/AllBillingProfileUserReadEntity.java
+++ read-api/src/main/java/onlydust/com/marketplace/api/read/entities/billing_profile/AllBillingProfileUserReadEntity.java
@@ -3,7 +3,6 @@ package onlydust.com.marketplace.api.read.entities.billing_profile;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import onlydust.com.backoffice.api.contract.model.VerificationStatus;
 import onlydust.com.marketplace.api.contract.model.BillingProfileCoworkerRole;
 import onlydust.com.marketplace.api.contract.model.ShortBillingProfileResponse;
 import org.hibernate.annotations.Immutable;
@@ -30,8 +29,9 @@ public class AllBillingProfileUserReadEntity {
 
     UUID userId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "billingProfileId", insertable = false, updatable = false)
+    @NonNull
     BillingProfileReadEntity billingProfile;
 
     @Enumerated(EnumType.STRING)
@@ -40,21 +40,7 @@ public class AllBillingProfileUserReadEntity {
     boolean invitationAccepted;
 
     public ShortBillingProfileResponse toShortResponse() {
-        return new ShortBillingProfileResponse()
-                .id(billingProfileId)
-                .name(billingProfile.name())
-                .enabled(billingProfile.enabled())
-                .invoiceMandateAccepted(!billingProfile.stats().mandateAcceptanceOutdated())
-                .pendingInvitationResponse(!invitationAccepted)
-                .invoiceableRewardCount(billingProfile.stats().invoiceableRewardCount())
-                .requestableRewardCount(role == BillingProfileCoworkerRole.ADMIN ? billingProfile.stats().invoiceableRewardCount() : 0)
-                .rewardCount(billingProfile.stats().rewardCount())
-                .missingPayoutInfo(billingProfile.stats().missingPayoutInfo())
-                .missingVerification(billingProfile.stats().missingVerification())
-                .individualLimitReached(billingProfile.stats().individualLimitReached())
-                .verificationBlocked(billingProfile.verificationStatus() == VerificationStatus.REJECTED || billingProfile.verificationStatus() == VerificationStatus.CLOSED)
-                .role(role)
-                .type(onlydust.com.marketplace.api.contract.model.BillingProfileType.valueOf(billingProfile.type().name()));
+        return billingProfile.toShortResponse(githubUserId);
     }
 
     @EqualsAndHashCode
