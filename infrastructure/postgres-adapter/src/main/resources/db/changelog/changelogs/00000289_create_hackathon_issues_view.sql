403779a78555d038cfde4e9862cdc7ccda3fe797
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-16 16:40:37 +0200

    Refactor by introducing hackathon_issues view

---
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000289_create_hackathon_issues_view.sql
@@ -0,0 +1,10 @@
+create view hackathon_issues as
+SELECT DISTINCT h.id as hackathon_id,
+                i.id as issue_id
+FROM indexer_exp.github_issues i
+         JOIN project_github_repos pgr ON pgr.github_repo_id = i.repo_id
+         JOIN indexer_exp.github_issues_labels gil ON i.id = gil.issue_id
+         JOIN indexer_exp.github_labels gl ON gil.label_id = gl.id
+         JOIN hackathon_projects hp ON hp.project_id = pgr.project_id
+         JOIN hackathons h ON gl.name = ANY (h.github_labels) AND hp.hackathon_id = h.id
+;
--- read-api/src/main/java/onlydust/com/marketplace/api/read/repositories/HackathonProjectIssuesReadRepository.java
+++ read-api/src/main/java/onlydust/com/marketplace/api/read/repositories/HackathonProjectIssuesReadRepository.java
@@ -1,5 +1,6 @@
 package onlydust.com.marketplace.api.read.repositories;
 
+import lombok.NonNull;
 import onlydust.com.marketplace.api.read.entities.hackathon.HackathonProjectIssuesReadEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
@@ -18,16 +19,14 @@ public interface HackathonProjectIssuesReadRepository extends Repository<Hackath
             FROM indexer_exp.github_issues i
                      JOIN project_github_repos pgr ON pgr.github_repo_id = i.repo_id
                      JOIN projects p ON p.id = pgr.project_id
+                     JOIN hackathon_issues h ON h.issue_id = i.id
                      LEFT JOIN repo_languages rl ON rl.repo_id = i.repo_id
-                     JOIN indexer_exp.github_issues_labels gil_hackathon ON i.id = gil_hackathon.issue_id
-                     JOIN indexer_exp.github_labels gl_hackathon ON gil_hackathon.label_id = gl_hackathon.id
-                     JOIN hackathon_projects hp ON hp.project_id = pgr.project_id
-                     JOIN hackathons h ON gl_hackathon.name = ANY (h.github_labels) AND hp.hackathon_id = h.id
                      LEFT JOIN indexer_exp.github_issues_assignees gia ON gia.issue_id = i.id
                      LEFT JOIN indexer_exp.github_issues_labels gil ON i.id = gil.issue_id
                      LEFT JOIN indexer_exp.github_labels gl ON gil.label_id = gl.id
                      LEFT JOIN applications a ON a.issue_id = i.id AND a.project_id = pgr.project_id
-            WHERE (coalesce(:statuses) IS NULL OR i.status = ANY (cast(:statuses as indexer_exp.github_issue_status[])))
+            WHERE h.hackathon_id = :hackathonId
+              AND (coalesce(:statuses) IS NULL OR i.status = ANY (cast(:statuses as indexer_exp.github_issue_status[])))
               AND (:isAssigned IS NULL
                 OR :isAssigned = TRUE AND gia.user_id IS NOT NULL
                 OR :isAssigned = FALSE AND gia.user_id IS NULL)
@@ -37,12 +36,11 @@ public interface HackathonProjectIssuesReadRepository extends Repository<Hackath
               AND (:isGoodFirstIssue IS NULL
                 OR :isGoodFirstIssue = TRUE AND gl.name ILIKE '%good%first%issue%'
                 OR :isGoodFirstIssue = FALSE AND gl.name NOT ILIKE '%good%first%issue%')
-              AND (:hackathonId IS NULL OR h.id = :hackathonId)
               AND (coalesce(:languageIds) IS NULL OR rl.language_id = ANY (:languageIds))
               AND (coalesce(:search) IS NULL OR i.title ILIKE '%' || :search || '%')
             GROUP BY p.id
             """, nativeQuery = true)
-    List<HackathonProjectIssuesReadEntity> findAll(UUID hackathonId,
+    List<HackathonProjectIssuesReadEntity> findAll(@NonNull UUID hackathonId,
                                                    String[] statuses,
                                                    Boolean isAssigned,
                                                    Boolean isApplied,
