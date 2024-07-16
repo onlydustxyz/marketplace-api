caba575e620b51449883f869fe23687d6a5086bf
PierreOucif <pierre.oucif@gadz.org>
2024-07-16 08:51:24 +0200

    Activate DBM in datadog prerun script

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-DB_HOST=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d ':' -f 1);
-DB_USERNAME=$(echo $DATABASE_URL | cut -d '/' -f 3 | cut -d ':' -f 1);
-DB_PASSWORD=$(echo $DATABASE_URL | cut -d '/' -f 3 | cut -d ':' -f 2 | cut -d '@' -f 1);
-DB_PORT=$(echo $DATABASE_URL | cut -d ':' -f 4 | cut -d '/' -f 1);
-DB_NAME=$(echo $DATABASE_URL | cut -d ':' -f 4 | cut -d '/' -f 2);
+function activate_datadog_database_monitoring() {
+  DB_HOST=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d ':' -f 1);
+  DB_USERNAME=$(echo $DATABASE_URL | cut -d '/' -f 3 | cut -d ':' -f 1);
+  DB_PASSWORD=$(echo $DATABASE_URL | cut -d '/' -f 3 | cut -d ':' -f 2 | cut -d '@' -f 1);
+  DB_PORT=$(echo $DATABASE_URL | cut -d ':' -f 4 | cut -d '/' -f 1);
+  DB_NAME=$(echo $DATABASE_URL | cut -d ':' -f 4 | cut -d '/' -f 2);
 
-echo "
-init_config:
-instances:
-  - host: ${DB_HOST}
-    username: ${DB_USERNAME}
-    password: ${DB_PASSWORD}
-    dbm: true
-    port: ${DB_PORT}
-    dbname: ${DB_NAME}
-    ssl: require
-    collect_schemas:
-      enabled: true
-" >> /app/.apt/etc/datadog-agent/conf.d/postgres.d/conf.yaml;
+  echo "
+  init_config:
+  instances:
+    - host: ${DB_HOST}
+      username: ${DB_USERNAME}
+      password: ${DB_PASSWORD}
+      dbm: true
+      port: ${DB_PORT}
+      dbname: ${DB_NAME}
+      ssl: require
+      collect_schemas:
+        enabled: true
+  " >> /app/.apt/etc/datadog-agent/conf.d/postgres.d/conf.yaml;
+}
+
+if [ "$ACTIVATE_DATADOG_DBM" == "true" ]; then
+  activate_datadog_database_monitoring()
+else
