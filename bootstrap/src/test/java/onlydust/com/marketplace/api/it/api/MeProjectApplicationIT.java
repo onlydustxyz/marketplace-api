0f1d50327ec7b4816abb20ae89c072c520062d9b
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-17 11:19:39 +0200

    Add (failing) IT to check that an external application to an ODHack issue is ignored

--- bootstrap/src/test/java/onlydust/com/marketplace/api/it/api/MeProjectApplicationIT.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/it/api/MeProjectApplicationIT.java
@@ -1,6 +1,7 @@
 package onlydust.com.marketplace.api.it.api;
 
 import com.onlydust.customer.io.adapter.properties.CustomerIOProperties;
+import jakarta.persistence.EntityManager;
 import onlydust.com.marketplace.api.contract.model.ProjectApplicationCreateRequest;
 import onlydust.com.marketplace.api.contract.model.ProjectApplicationCreateResponse;
 import onlydust.com.marketplace.api.contract.model.ProjectApplicationUpdateRequest;
@@ -14,6 +15,8 @@ import onlydust.com.marketplace.kernel.jobs.OutboxConsumerJob;
 import onlydust.com.marketplace.kernel.model.event.OnGithubCommentCreated;
 import onlydust.com.marketplace.kernel.model.event.OnGithubIssueDeleted;
 import onlydust.com.marketplace.project.domain.model.Application;
+import onlydust.com.marketplace.project.domain.model.Hackathon;
+import onlydust.com.marketplace.project.domain.port.output.HackathonStoragePort;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +30,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static onlydust.com.marketplace.api.rest.api.adapter.authentication.AuthenticationFilter.BEARER_PREFIX;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @TagMe
@@ -43,6 +47,8 @@ public class MeProjectApplicationIT extends AbstractMarketplaceApiIT {
     PosthogProperties posthogProperties;
     @Autowired
     CustomerIOProperties customerIOProperties;
+    @Autowired
+    HackathonStoragePort hackathonStoragePort;
 
     @BeforeEach
     void setUp() {
@@ -536,6 +542,133 @@ public class MeProjectApplicationIT extends AbstractMarketplaceApiIT {
                 .withRequestBody(matchingJsonPath("$.properties['issue_id']", equalTo(issueId.toString())))
                 .withRequestBody(matchingJsonPath("$.properties['applicant_github_id']", equalTo(antho.user().getGithubUserId().toString())))
                 .withRequestBody(matchingJsonPath("$.properties['origin']", equalTo("GITHUB")))
+        );
+    }
+
+    @Test
+    void should_detect_github_application_on_odhack_issue() {
+        // Given
+        final var commentId = faker.number().randomNumber(10, true);
+        final Long issueId = 1914185678L;
+        final var repoId = 466482535L;
+        final var antho = userAuthHelper.authenticateAnthony();
+        final var commentBody = faker.lorem().sentence();
+
+        final var startDate = ZonedDateTime.of(2024, 4, 19, 0, 0, 0, 0, ZonedDateTime.now().getZone());
+        final var hackathon1 = Hackathon.builder()
+                .id(Hackathon.Id.of("200cfa00-b1f9-45f4-96f4-0e53874a327e"))
+                .title("Hackathon 1")
+                .startDate(startDate)
+                .endDate(startDate.plusDays(1))
+                .status(Hackathon.Status.PUBLISHED)
+                .build();
+        hackathon1.githubLabels().add("Hackathon-1");
+        hackathon1.projectIds().add(UUID.fromString("7d04163c-4187-4313-8066-61504d34fc56"));
+        hackathonStoragePort.save(hackathon1);
+
+        final EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.createNativeQuery("""
+                INSERT INTO indexer_exp.github_labels (id, name) VALUES (987654321, 'Hackathon-1');
+                INSERT INTO indexer_exp.github_issues_labels (issue_id, label_id) VALUES (1914185678, 987654321);
+                DELETE FROM indexer_exp.github_issues_assignees WHERE issue_id = 1914185678;
+                """).executeUpdate();
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
+
+        indexingEventRepository.saveEvent(OnGithubCommentCreated.builder()
+                .id(commentId)
+                .issueId(issueId)
+                .repoId(repoId)
+                .authorId(antho.user().getGithubUserId())
+                .createdAt(ZonedDateTime.now().minusSeconds(2))
+                .body(commentBody)
+                .build());
+        indexingEventRepository.flush();
+
+        langchainWireMockServer.stubFor(post(urlEqualTo("/chat/completions"))
+                .withHeader("Authorization", equalTo("Bearer OPENAI_API_KEY"))
+                .withRequestBody(matchingJsonPath("model", equalTo("gpt-3.5-turbo")))
+                .withRequestBody(matchingJsonPath("messages[?(@.role == 'user')].content", containing(commentBody)))
+                .withRequestBody(matchingJsonPath("temperature", equalTo("0.7")))
+                .willReturn(okJson("""
+                        {
+                          "id": "chatcmpl-9cUTl59knClLSGpf1HmnnP3hyUeZz",
+                          "created": 1718960654,
+                          "model": "gpt-3.5-turbo-0125",
+                          "choices": [
+                            {
+                              "index": 0,
+                              "message": {
+                                "role": "assistant",
+                                "content": "true"
+                              },
+                              "finish_reason": "stop"
+                            }
+                          ],
+                          "usage": {
+                            "prompt_tokens": 56,
+                            "completion_tokens": 1,
+                            "total_tokens": 57
+                          }
+                        }
+                        """)));
+
+        githubWireMockServer.stubFor(post(urlEqualTo("/app/installations/44637372/access_tokens"))
+                .withHeader("Authorization", matching("Bearer .*"))
+                .willReturn(aResponse()
+                        .withStatus(201)
+                        .withBody("""
+                                {
+                                    "token": "GITHUB_APP_PERSONAL_ACCESS_TOKEN",
+                                    "permissions": {
+                                        "issues": "write"
+                                    }
+                                }
+                                """)
+                ));
+
+        final var expectedComment = """
+                Hey @AnthonyBuisset!\\n\
+                Thanks for showing interest.\\n\
+                This issue is part of the OnlyDust hackathon Hackathon 1. Please apply through OnlyDust to get a chance to be assigned!\\n\
+                Go check it out on [OnlyDust](https://local-app.onlydust.com/h/hackathon-1)!\\n\
+                """;
+
+        githubWireMockServer.stubFor(post(urlEqualTo("/repositories/466482535/issues/1/comments"))
+                .withHeader("Authorization", matching("Bearer GITHUB_APP_PERSONAL_ACCESS_TOKEN"))
+                .withRequestBody(equalToJson("""
+                        {
+                            "body": "%s"
+                        }
+                        """.formatted(expectedComment)))
+                .willReturn(aResponse()
+                        .withStatus(201)
+                        .withBody("""
+                                {
+                                    "id": 123456789
+                                }
+                                """)
+                ));
+
+        // When
+        indexingEventsOutboxJob.run();
+
+        // Then
+        final var applications = applicationRepository.findAllByCommentId(commentId);
+        assertThat(applications).hasSize(0);
+
+        indexerApiWireMockServer.verify(putRequestedFor(urlEqualTo("/api/v1/users/" + antho.user().getGithubUserId())));
+        githubWireMockServer.verify(postRequestedFor(urlEqualTo("/repositories/466482535/issues/1/comments")));
+        verify(slackApiAdapter, never()).onApplicationCreated(any(Application.class));
+
+        trackingOutboxJob.run();
+
+        posthogWireMockServer.verify(0, postRequestedFor(urlEqualTo("/capture/"))
+                .withHeader("Content-Type", equalTo("application/json"))
+                .withRequestBody(matchingJsonPath("$.api_key", equalTo(posthogProperties.getApiKey())))
+                .withRequestBody(matchingJsonPath("$.event", equalTo("issue_applied")))
         );
     }
 
