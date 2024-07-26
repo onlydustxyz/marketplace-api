17f9f485415524ad816f19663b033d40cc66b11d
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-26 17:45:30 +0200

    Add notifications IT

--- bootstrap/src/test/java/onlydust/com/marketplace/api/it/api/feature/NotificationsIT.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/it/api/feature/NotificationsIT.java
@@ -3,7 +3,6 @@ package onlydust.com.marketplace.api.it.api.feature;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import onlydust.com.marketplace.api.helper.UserAuthHelper;
 import onlydust.com.marketplace.api.it.api.AbstractMarketplaceApiIT;
 import onlydust.com.marketplace.api.suites.tags.TagUser;
 import onlydust.com.marketplace.kernel.model.notification.*;
@@ -28,21 +27,27 @@ public class NotificationsIT extends AbstractMarketplaceApiIT {
     @Autowired
     NotificationPort notificationPort;
 
-    private UserAuthHelper.AuthenticatedUser olivier;
     private UUID olivierId;
     private NotificationRecipient olivierRecipient;
+    private UUID pierreId;
+    private NotificationRecipient pierreRecipient;
 
     private final TestNotification rewardNotification1 = new TestNotification(1, NotificationCategory.REWARD_AS_CONTRIBUTOR);
     private final TestNotification rewardNotification2 = new TestNotification(2, NotificationCategory.REWARD_AS_CONTRIBUTOR);
     private final TestNotification rewardNotification3 = new TestNotification(3, NotificationCategory.REWARD_AS_CONTRIBUTOR);
+    private final TestNotification rewardNotification4 = new TestNotification(4, NotificationCategory.REWARD_AS_CONTRIBUTOR);
     private final TestNotification gfiNotification1 = new TestNotification(100, NotificationCategory.PROJECT_GOOD_FIRST_ISSUE_AS_CONTRIBUTOR);
     private final TestNotification gfiNotification2 = new TestNotification(101, NotificationCategory.PROJECT_GOOD_FIRST_ISSUE_AS_CONTRIBUTOR);
 
     @BeforeEach
     void setUp() {
-        olivier = userAuthHelper.authenticateOlivier();
+        final var olivier = userAuthHelper.authenticateOlivier();
         olivierId = olivier.user().getId();
         olivierRecipient = new NotificationRecipient(olivierId, olivier.user().getGithubEmail(), olivier.user().getGithubLogin());
+
+        final var pierre = userAuthHelper.authenticatePierre();
+        pierreId = pierre.user().getId();
+        pierreRecipient = new NotificationRecipient(pierreId, pierre.user().getGithubEmail(), pierre.user().getGithubLogin());
     }
 
     @Test
@@ -190,6 +195,38 @@ public class NotificationsIT extends AbstractMarketplaceApiIT {
         ), NotificationChannel.IN_APP);
         assertPendingNotifications(Map.of(
                 olivierRecipient, List.of(gfiNotification1, gfiNotification2)
+        ), NotificationChannel.EMAIL);
+    }
+
+    @Test
+    @Order(30)
+    void should_not_mix_notifications_between_users() {
+        // Given
+        notificationSettingsPort.updateNotificationSettings(UserId.of(pierreId), NotificationSettings.builder()
+                .channelsPerCategory(Map.of(
+                        NotificationCategory.REWARD_AS_CONTRIBUTOR, List.of(NotificationChannel.EMAIL, NotificationChannel.IN_APP),
+                        NotificationCategory.PROJECT_GOOD_FIRST_ISSUE_AS_CONTRIBUTOR, List.of()
+                ))
+                .build());
+
+        // When
+        notificationPort.push(pierreId, rewardNotification1);
+        notificationPort.push(pierreId, rewardNotification4);
+        notificationPort.push(pierreId, gfiNotification1);
+        notificationPort.push(olivierId, rewardNotification4);
+
+        // Then
+        assertPendingNotifications(Map.of(
+                olivierRecipient, List.of(rewardNotification1, rewardNotification2, gfiNotification2)
+        ), NotificationChannel.DAILY_EMAIL);
+        assertPendingNotifications(Map.of(
+                olivierRecipient, List.of(rewardNotification1, rewardNotification2, gfiNotification1, gfiNotification2, rewardNotification3,
+                        rewardNotification4),
+                pierreRecipient, List.of(rewardNotification1, rewardNotification4)
+        ), NotificationChannel.IN_APP);
+        assertPendingNotifications(Map.of(
+                olivierRecipient, List.of(gfiNotification1, gfiNotification2),
+                pierreRecipient, List.of(rewardNotification1, rewardNotification4)
         ), NotificationChannel.EMAIL);
     }
 
