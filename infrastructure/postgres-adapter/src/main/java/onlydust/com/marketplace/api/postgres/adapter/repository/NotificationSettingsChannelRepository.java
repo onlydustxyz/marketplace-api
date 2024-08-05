de4c4fd7198af7eca3380dfe07cebe1e84071f9e
Olivier Fuxet <ofux@users.noreply.github.com>
2024-08-05 09:42:41 +0200

    Should fix error: HHH100501: Exception executing batch [org.hibernate.StaleStateException: Batch update returned unexpected row count from update [0]; actual row count: 0; expected: 1; statement executed: delete from iam.user_notification_settings_channels where category=? and channel=? and user_id=?], SQL: delete from iam.user_notification_settings_channels where category=? and channel=? and user_id=? (#974)

--- infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/NotificationSettingsChannelRepository.java
+++ infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/NotificationSettingsChannelRepository.java
@@ -3,6 +3,7 @@ package onlydust.com.marketplace.api.postgres.adapter.repository;
 import onlydust.com.marketplace.api.postgres.adapter.entity.write.NotificationSettingsChannelEntity;
 import onlydust.com.marketplace.kernel.model.notification.NotificationCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,5 +12,6 @@ public interface NotificationSettingsChannelRepository extends JpaRepository<Not
 
     List<NotificationSettingsChannelEntity> findAllByUserIdAndCategory(UUID userId, NotificationCategory category);
 
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
     void deleteAllByUserId(UUID userId);
 }
