7af1816e86094a5a3aa391abd50053ab1e93f2c5
Ilysse <pierre.oucif@gadz.org>
2024-08-12 16:34:38 +0200

    Move customer io email ids to env vars (#998)

---
+++ infrastructure/sumsub-api-client-adapter/src/main/java/com/onlydust/api/sumsub/api/client/adapter/dto/SumsubIndividualApplicantsByApplicantIdDataDTO.java
@@ -0,0 +1,18 @@
+package com.onlydust.api.sumsub.api.client.adapter.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SumsubIndividualApplicantsByApplicantIdDataDTO {
+    String email;
+}
