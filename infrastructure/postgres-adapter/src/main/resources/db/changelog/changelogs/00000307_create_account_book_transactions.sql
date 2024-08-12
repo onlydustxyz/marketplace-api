0e2aadd52f0862ae4301668f3b7984ed991ecdb8
Anthony Buisset <abuisset@gmail.com>
2024-08-12 09:55:31 +0200

    defer fk

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000307_create_account_book_transactions.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000307_create_account_book_transactions.sql
@@ -2,7 +2,7 @@ create table accounting.account_book_transactions
 (
     index              bigserial primary key,
     timestamp          timestamp                   not null,
-    account_book_id    uuid                        not null references accounting.account_books (id),
+    account_book_id    uuid                        not null references accounting.account_books (id) deferrable initially deferred,
     type               accounting.transaction_type not null,
     sponsor_account_id uuid,
     project_id         uuid,
