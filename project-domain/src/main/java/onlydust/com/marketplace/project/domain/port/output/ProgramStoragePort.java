c717d959b9a50b9e456e9f90f33fe4f47ff0812a
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-08-27 10:48:51 +0200

    allow program lead to see sponsored private projects (#1044)

--- project-domain/src/main/java/onlydust/com/marketplace/project/domain/port/output/ProgramStoragePort.java
+++ project-domain/src/main/java/onlydust/com/marketplace/project/domain/port/output/ProgramStoragePort.java
@@ -1,6 +1,7 @@
 package onlydust.com.marketplace.project.domain.port.output;
 
 import onlydust.com.marketplace.kernel.model.ProgramId;
+import onlydust.com.marketplace.kernel.model.ProjectId;
 import onlydust.com.marketplace.kernel.model.UserId;
 import onlydust.com.marketplace.project.domain.model.Program;
 
@@ -9,7 +10,11 @@ import java.util.UUID;
 public interface ProgramStoragePort {
     boolean isAdmin(UUID userId, ProgramId programId);
 
+    boolean isAdmin(UUID userId, ProjectId programId);
+
     void save(Program program);
 
     void saveProgramLead(ProgramId programId, UserId leadId);
+
+    boolean isAdmin(UUID userId, String projectSlug);
 }
