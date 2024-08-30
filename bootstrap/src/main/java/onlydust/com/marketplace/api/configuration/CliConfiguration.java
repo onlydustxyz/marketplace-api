a224edcd8ee3929891e3c6f53252e26b3faf3dc8
Anthony Buisset <abuisset@gmail.com>
2024-08-30 17:45:26 +0200

    remove unused binary

--- bootstrap/src/main/java/onlydust/com/marketplace/api/configuration/CliConfiguration.java
+++ bootstrap/src/main/java/onlydust/com/marketplace/api/configuration/CliConfiguration.java
@@ -7,10 +7,8 @@ import onlydust.com.marketplace.accounting.domain.port.out.CurrencyStorage;
 import onlydust.com.marketplace.accounting.domain.port.out.SponsorAccountStorage;
 import onlydust.com.marketplace.accounting.domain.service.CachedAccountBookProvider;
 import onlydust.com.marketplace.api.postgres.adapter.repository.AccountBookTransactionRepository;
-import onlydust.com.marketplace.api.postgres.adapter.repository.WalletRepository;
 import onlydust.com.marketplace.cli.AccountBookDisplay;
 import onlydust.com.marketplace.cli.AccountBookRefresh;
-import onlydust.com.marketplace.cli.EvmWalletSanitizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -32,11 +30,4 @@ public class CliConfiguration {
                                                  final AccountBookTransactionRepository accountBookTransactionRepository) {
         return new AccountBookRefresh(currencyStorage, accountBookStorage, accountBookEventStorage, accountBookProjector, accountBookTransactionRepository);
     }
-
-
-    @Bean
-    public EvmWalletSanitizer evmWalletSanitizer(final WalletRepository walletRepository) {
-        return new EvmWalletSanitizer(walletRepository);
-    }
-
 }
