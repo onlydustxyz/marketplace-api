b6971303185fbe9984f8f932baecf4ac913922a6
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-08-30 18:26:44 +0200

    add starknet transfer transaction support (#1080)
    test

--- bootstrap/src/test/java/onlydust/com/marketplace/api/helper/CurrencyHelper.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/helper/CurrencyHelper.java
@@ -2,8 +2,14 @@ package onlydust.com.marketplace.api.helper;
 
 import lombok.AllArgsConstructor;
 import onlydust.com.marketplace.accounting.domain.model.Currency;
+import onlydust.com.marketplace.accounting.domain.port.in.CurrencyFacadePort;
+import onlydust.com.marketplace.kernel.model.blockchain.Blockchain;
+import onlydust.com.marketplace.kernel.model.blockchain.StarkNet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
+@Service
 public class CurrencyHelper {
 
     public static final Currency.Id BTC = Currency.Id.of("3f6e1c98-8659-493a-b941-943a803bd91f");
@@ -13,4 +19,17 @@ public class CurrencyHelper {
     public static final Currency.Id OP = Currency.Id.of("00ca98a5-0197-4b76-a208-4bfc55ea8256");
     public static final Currency.Id ETH = Currency.Id.of("71bdfcf4-74ee-486b-8cfe-5d841dd93d5c");
     public static final Currency.Id APT = Currency.Id.of("48388edb-fda2-4a32-b228-28152a147500");
+
+    @Autowired
+    private final CurrencyFacadePort currencyFacadePort;
+
+    public Currency addERC20Support(Blockchain blockchain, Currency.Code code) {
+        return currencyFacadePort.addERC20Support(blockchain, switch (blockchain) {
+            case STARKNET -> switch (code.toString()) {
+                case Currency.Code.USDC_STR -> StarkNet.contractAddress("0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8");
+                default -> throw new IllegalArgumentException("Unsupported ERC20 token in tests");
+            };
+            default -> throw new IllegalArgumentException("Unsupported blockchain in tests");
+        });
+    }
 }
