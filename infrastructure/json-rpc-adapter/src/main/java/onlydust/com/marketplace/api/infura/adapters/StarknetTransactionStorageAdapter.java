2c7af435f92510f79320e4dd1e67a5dae58c7699
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-08-30 18:26:44 +0200

    add starknet transfer transaction support (#1080)
    test

--- infrastructure/json-rpc-adapter/src/main/java/onlydust/com/marketplace/api/infura/adapters/StarknetTransactionStorageAdapter.java
+++ infrastructure/json-rpc-adapter/src/main/java/onlydust/com/marketplace/api/infura/adapters/StarknetTransactionStorageAdapter.java
@@ -1,19 +1,26 @@
 package onlydust.com.marketplace.api.infura.adapters;
 
+import com.swmansion.starknet.data.types.BlockWithTransactionHashes;
+import com.swmansion.starknet.data.types.Event;
 import com.swmansion.starknet.data.types.Felt;
 import com.swmansion.starknet.data.types.TransactionReceipt;
 import com.swmansion.starknet.provider.Provider;
 import com.swmansion.starknet.provider.rpc.JsonRpcProvider;
+import lombok.NonNull;
 import onlydust.com.marketplace.accounting.domain.port.out.BlockchainTransactionStoragePort;
 import onlydust.com.marketplace.api.infura.Web3Client;
 import onlydust.com.marketplace.kernel.model.blockchain.Blockchain;
 import onlydust.com.marketplace.kernel.model.blockchain.StarkNet;
 import onlydust.com.marketplace.kernel.model.blockchain.starknet.StarknetTransaction;
+import onlydust.com.marketplace.kernel.model.blockchain.starknet.StarknetTransferTransaction;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.Optional;
 
+import static onlydust.com.marketplace.kernel.exception.OnlyDustException.internalServerError;
 import static onlydust.com.marketplace.kernel.model.blockchain.Blockchain.Transaction.Status.*;
 
 public class StarknetTransactionStorageAdapter implements BlockchainTransactionStoragePort<StarknetTransaction, StarknetTransaction.Hash> {
@@ -29,11 +36,31 @@ public class StarknetTransactionStorageAdapter implements BlockchainTransactionS
 
         return Optional.ofNullable(receipt.getBlockHash())
                 .map(blockHash -> provider.getBlockWithTxHashes(blockHash).send())
-                .map(block -> new StarknetTransaction(
-                        StarkNet.transactionHash(receipt.getHash().hexString()),
-                        Instant.ofEpochSecond(block.getTimestamp()).atZone(ZoneOffset.UTC),
-                        statusOf(receipt))
-                );
+                .flatMap(block -> receipt.getEvents().stream()
+                        .map(event -> fromEvent(block, receipt, event))
+                        .findFirst());
+    }
+
+    private @NonNull StarknetTransferTransaction fromEvent(final @NonNull BlockWithTransactionHashes block,
+                                                           final @NonNull TransactionReceipt receipt,
+                                                           final @NonNull Event event) {
+        try {
+            final var contractAddress = StarkNet.contractAddress(event.getAddress().hexString());
+            final var erc20 = StarknetERC20ProviderAdapter.ERC20Contract.load(provider, contractAddress);
+            final var decimals = erc20.decimals().get();
+
+            return new StarknetTransferTransaction(
+                    StarkNet.transactionHash(receipt.getHash().hexString()),
+                    Instant.ofEpochSecond(block.getTimestamp()).atZone(ZoneOffset.UTC),
+                    statusOf(receipt),
+                    StarkNet.accountAddress(event.getData().get(0).hexString()),
+                    StarkNet.accountAddress(event.getData().get(1).hexString()),
+                    new BigDecimal(new BigInteger(event.getData().get(2).decString()), decimals.intValue()),
+                    contractAddress
+            );
+        } catch (Exception e) {
+            throw internalServerError("Unable to fetch ERC20 decimals", e);
+        }
     }
 
     private Blockchain.Transaction.Status statusOf(TransactionReceipt receipt) {
