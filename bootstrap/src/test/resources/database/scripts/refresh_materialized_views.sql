5a364c8971849ab47bc5ff0b463df2628a84f4d5
PierreOucif <pierre.oucif@gadz.org>
2024-09-03 16:00:57 +0200

    Fix IT

--- bootstrap/src/test/resources/database/scripts/refresh_materialized_views.sql
+++ bootstrap/src/test/resources/database/scripts/refresh_materialized_views.sql
@@ -13,3 +13,5 @@ REFRESH MATERIALIZED VIEW global_users_ranks;
 REFRESH MATERIALIZED VIEW repo_languages;
 REFRESH MATERIALIZED VIEW project_languages;
 REFRESH MATERIALIZED VIEW top_project_recommendations;
+REFRESH MATERIALIZED VIEW bi.project_contribution_stats;
+REFRESH MATERIALIZED VIEW bi.project_reward_stats;
