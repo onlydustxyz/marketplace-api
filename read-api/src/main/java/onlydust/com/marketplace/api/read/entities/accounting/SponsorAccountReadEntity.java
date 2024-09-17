9a3db7abd7019dc9a469680dccc1335ad0e95f72
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-09-17 17:15:40 +0200

    remove unused BO endpoints (#1137)

--- read-api/src/main/java/onlydust/com/marketplace/api/read/entities/accounting/SponsorAccountReadEntity.java
+++ read-api/src/main/java/onlydust/com/marketplace/api/read/entities/accounting/SponsorAccountReadEntity.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldDefaults;
-import onlydust.com.backoffice.api.contract.model.AccountResponse;
-import onlydust.com.marketplace.accounting.domain.model.SponsorAccountStatement;
 import onlydust.com.marketplace.api.read.entities.currency.CurrencyReadEntity;
 import onlydust.com.marketplace.api.read.entities.sponsor.SponsorReadEntity;
 import org.hibernate.annotations.Immutable;
@@ -16,7 +14,6 @@ import java.util.UUID;
 
 @Entity
 @NoArgsConstructor(force = true)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Table(name = "sponsor_accounts", schema = "accounting")
@@ -46,20 +43,4 @@ public class SponsorAccountReadEntity {
     @OrderBy("timestamp DESC")
     @NonNull
     List<SponsorAccountTransactionReadEntity> transactions;
-
-    public AccountResponse toDto(SponsorAccountStatement sponsorAccountStatement) {
-        return new AccountResponse()
-                .id(id)
-                .sponsorId(sponsorId)
-                .lockedUntil(lockedUntil)
-                .receipts(transactions.stream().map(SponsorAccountTransactionReadEntity::toDto).toList())
-                .currency(currency.toBoShortResponse())
-                .initialBalance(sponsorAccountStatement.account().initialBalance().getValue())
-                .currentBalance(sponsorAccountStatement.account().balance().getValue())
-                .initialAllowance(sponsorAccountStatement.initialAllowance().getValue())
-                .currentAllowance(sponsorAccountStatement.allowance().getValue())
-                .debt(sponsorAccountStatement.debt().getValue())
-                .awaitingPaymentAmount(sponsorAccountStatement.awaitingPaymentAmount().getValue())
-                ;
-    }
 }
