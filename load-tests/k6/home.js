f7215526225ed2d9540159a6fef90f7dc35903e1
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-09-27 14:38:45 +0200

    Update k6 test

--- load-tests/k6/home.js
+++ load-tests/k6/home.js
@@ -1,3 +1,5 @@
+// Run with: K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=html-report.html k6 run load-tests/k6/home.js
+
 import http from 'k6/http';
 
 export const options = {
@@ -5,14 +7,15 @@ export const options = {
         display_homepage: {
             executor: 'per-vu-iterations',
             vus: 20,
-            iterations: 20,
+            iterations: 100,
             startTime: '5s',
         },
     },
 };
 
+const BASE_URL = 'https://develop-api.onlydust.com/api/v1';
+
 export default function () {
-    http.get('https://api.onlydust.com/api/v1/projects');
-    http.get('https://api.onlydust.com/api/v1/public-activity?pageIndex=0&pageSize=10');
-    http.get('https://api.onlydust.com/api/v1/hackathons');
+    http.get(`${BASE_URL}/projects`);
+    http.get(`${BASE_URL}/public-activity?pageIndex=0&pageSize=10`);
 }
