35facd480c9372cd720d17fd9ea14492a2f1e475
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-10-03 14:08:37 +0200

    Fix refresh_pseudo_projection

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000356_project_deposits_into_all_transactions.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000356_project_deposits_into_all_transactions.sql
@@ -13,3 +13,32 @@ SELECT d.id,
 FROM accounting.deposits d
          join accounting.transfer_transactions tt on tt.id = d.transaction_id
 where d.status != 'DRAFT';
+
+
+
+CREATE OR REPLACE PROCEDURE refresh_pseudo_projection(schema text, name text, params jsonb)
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    view_name             text;
+    projection_table_name text;
+    condition             text;
+    key                   text;
+    value                 text;
+BEGIN
+    view_name := 'v_' || name;
+    projection_table_name := 'p_' || name;
+
+    condition := '';
+    FOR key, value IN SELECT * FROM jsonb_each_text(params)
+        LOOP
+            condition := condition || format(' and %I = %L', key, value);
+        END LOOP;
+
+    EXECUTE format('delete from %I.%I where true %s', schema, projection_table_name, condition);
+
+    EXECUTE format('insert into %I.%I select * from %I.%I where true %s on conflict do nothing', schema, projection_table_name, schema, view_name,
+                   condition);
+END
+$$;
