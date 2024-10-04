eaa35eb56036e4195e8fb314b6eab9ba7afae076
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-10-04 23:58:20 +0200

    Improve v_contribution_data perf

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000359_improve_perf_of_refreshes.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000359_improve_perf_of_refreshes.sql
@@ -87,21 +87,15 @@ FROM (SELECT c.*,
 
 CREATE OR REPLACE VIEW bi.v_contribution_data AS
 SELECT v.*, md5(v::text) as hash
-from (with ranked_project_github_repos_relationship AS (SELECT *, row_number() OVER (PARTITION BY github_repo_id ORDER BY project_id) as row_number
-                                                        FROM project_github_repos),
-           registered_users as (select u.id             as id,
-                                       u.github_user_id as github_user_id,
-                                       kyc.country      as country
-                                from iam.users u
-                                         join accounting.billing_profiles_users bpu on bpu.user_id = u.id
-                                         join accounting.kyc on kyc.billing_profile_id = bpu.billing_profile_id)
+FROM (with ranked_project_github_repos_relationship AS (SELECT *, row_number() OVER (PARTITION BY github_repo_id ORDER BY project_id) as row_number
+                                                        FROM project_github_repos)
       select c.id                                                                                             as contribution_id,
              c.repo_id                                                                                        as repo_id,
              p.id                                                                                             as project_id,
              p.slug                                                                                           as project_slug,
              c.contributor_id                                                                                 as contributor_id,
-             ru.id                                                                                            as contributor_user_id,
-             ru.country                                                                                       as contributor_country,
+             u.id                                                                                             as contributor_user_id,
+             (array_agg(kyc.country) filter (where kyc.country is not null))[1]                               as contributor_country,
              c.created_at                                                                                     as timestamp,
              c.status                                                                                         as contribution_status,
              date_trunc('day', c.created_at)                                                                  as day_timestamp,
@@ -109,7 +103,10 @@ from (with ranked_project_github_repos_relationship AS (SELECT *, row_number() O
              date_trunc('month', c.created_at)                                                                as month_timestamp,
              date_trunc('quarter', c.created_at)                                                              as quarter_timestamp,
              date_trunc('year', c.created_at)                                                                 as year_timestamp,
-             c.created_at = first.created_at                                                                  as is_first_contribution_on_onlydust,
+             (select not exists(select 1
+                                from indexer_exp.contributions cc
+                                where cc.contributor_id = c.contributor_id
+                                  and cc.created_at < c.created_at))                                          as is_first_contribution_on_onlydust,
              (c.type = 'ISSUE')::int                                                                          as is_issue,
              (c.type = 'PULL_REQUEST')::int                                                                   as is_pr,
              (c.type = 'CODE_REVIEW')::int                                                                    as is_code_review,
@@ -121,10 +118,6 @@ from (with ranked_project_github_repos_relationship AS (SELECT *, row_number() O
       from indexer_exp.contributions c
                left join ranked_project_github_repos_relationship pgr on pgr.github_repo_id = c.repo_id and pgr.row_number = 1
                left join projects p on p.id = pgr.project_id
-               join lateral (select cc.contributor_id, min(cc.created_at) as created_at
-                             from indexer_exp.contributions cc
-                             where cc.contributor_id = c.contributor_id
-                             group by cc.contributor_id) first on true
                left join lateral ( select distinct lfe_1.language_id, l.name
                                    from language_file_extensions lfe_1
                                             join languages l on l.id = lfe_1.language_id
@@ -132,7 +125,9 @@ from (with ranked_project_github_repos_relationship AS (SELECT *, row_number() O
                left join projects_ecosystems pe on pe.project_id = p.id
                left join v_programs_projects pp on pp.project_id = p.id
                left join projects_project_categories ppc on ppc.project_id = p.id
-               left join registered_users ru on ru.github_user_id = c.contributor_id
+               left join iam.users u on u.github_user_id = c.contributor_id
+               left join accounting.billing_profiles_users bpu on bpu.user_id = u.id
+               left join accounting.kyc on kyc.billing_profile_id = bpu.billing_profile_id
       group by c.id,
                c.repo_id,
                p.id,
@@ -141,6 +136,6 @@ from (with ranked_project_github_repos_relationship AS (SELECT *, row_number() O
                c.created_at,
                c.type,
                c.status,
-               first.created_at,
-               ru.id,
-               ru.country) v;
+               u.id) v;
+
+create index on indexer_exp.contributions (contributor_id, created_at);
