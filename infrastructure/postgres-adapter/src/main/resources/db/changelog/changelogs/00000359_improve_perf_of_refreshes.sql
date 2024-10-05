d26b0919f1b94ff5b327da8c4515792f13a65d61
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-10-05 19:49:29 +0200

    Improve v_contributor_global_data perf

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000359_improve_perf_of_refreshes.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000359_improve_perf_of_refreshes.sql
@@ -25,6 +25,19 @@ FROM (SELECT c.*,
              bi.search_of(c.contributor_login, c.projects, c.categories, c.languages, c.ecosystems, c.currencies, c.programs) as search
 
       FROM (SELECT c.*,
+                   (select kyc.country
+                    from accounting.billing_profiles_users bpu
+                             join accounting.kyc on kyc.billing_profile_id = bpu.billing_profile_id and kyc.country is not null
+                    limit 1)                                    as contributor_country,
+
+                   (select jsonb_build_object('githubUserId', u.github_user_id,
+                                              'login', u.login,
+                                              'avatarUrl', u.avatar_url,
+                                              'isRegistered', u.user_id is not null,
+                                              'id',
+                                              u.user_id)
+                    from iam.all_users u
+                    where u.github_user_id = c.contributor_id)  as contributor,
 
                    (select jsonb_agg(jsonb_build_object('id', p.id,
                                                         'slug', p.slug,
@@ -69,24 +82,8 @@ FROM (SELECT c.*,
                     from currencies cu
                     where cu.id = any (c.currency_ids))         as currencies
 
-            FROM (WITH ranked_project_github_repos_relationship AS (SELECT project_github_repos.project_id,
-                                                                           project_github_repos.github_repo_id,
-                                                                           row_number()
-                                                                           OVER (PARTITION BY project_github_repos.github_repo_id ORDER BY project_github_repos.project_id) AS row_number
-                                                                    FROM project_github_repos),
-                       user_countries as (select bpu.user_id as user_id,
-                                                 kyc.country as country
-                                          from accounting.billing_profiles_users bpu
-                                                   join accounting.kyc on kyc.billing_profile_id = bpu.billing_profile_id)
-                  SELECT u.github_user_id                                                                                 as contributor_id,
-                         u.login                                                                                          as contributor_login,
-                         uc.country                                                                                       as contributor_country,
-                         jsonb_build_object('githubUserId', u.github_user_id,
-                                            'login', u.login,
-                                            'avatarUrl', u.avatar_url,
-                                            'isRegistered', u.user_id is not null,
-                                            'id',
-                                            u.user_id)                                                                    as contributor,
+            FROM (SELECT ga.id                                                                                            as contributor_id,
+                         ga.login                                                                                         as contributor_login,
 
                          min(p.name)                                                                                      as first_project_name,
 
@@ -97,26 +94,19 @@ FROM (SELECT c.*,
                          array_agg(distinct pe.ecosystem_id) filter ( where pe.ecosystem_id is not null )                 as ecosystem_ids,
                          array_agg(distinct pp.program_id) filter ( where pp.program_id is not null )                     as program_ids,
                          array_agg(distinct currencies.id) filter ( where currencies.id is not null )                     as currency_ids
-                  FROM iam.all_users u
-                           LEFT JOIN user_countries uc on uc.user_id = u.user_id
-                           LEFT JOIN indexer_exp.contributions c ON c.contributor_id = u.github_user_id
-                           LEFT JOIN ranked_project_github_repos_relationship pgr ON pgr.github_repo_id = c.repo_id AND pgr.row_number = 1
+                  FROM indexer_exp.github_accounts ga
+                           LEFT JOIN indexer_exp.contributions c ON c.contributor_id = ga.id
+                           LEFT JOIN project_github_repos pgr ON pgr.github_repo_id = c.repo_id
                            LEFT JOIN projects p on p.id = pgr.project_id
                            LEFT JOIN projects_ecosystems pe ON pe.project_id = p.id
                            LEFT JOIN v_programs_projects pp ON pp.project_id = p.id
                            LEFT JOIN projects_project_categories ppc ON ppc.project_id = p.id
-                           LEFT JOIN rewards r on r.recipient_id = u.github_user_id
+                           LEFT JOIN rewards r on r.recipient_id = ga.id
                            LEFT JOIN currencies on r.currency_id = currencies.id
-                           LEFT JOIN LATERAL ( SELECT DISTINCT lfe_1.language_id,
-                                                               l.name
-                                               FROM language_file_extensions lfe_1
-                                                        JOIN languages l ON l.id = lfe_1.language_id
-                                               WHERE lfe_1.extension = ANY (c.main_file_extensions)) lfe ON true
-                  GROUP BY u.github_user_id,
-                           u.login,
-                           u.avatar_url,
-                           u.user_id,
-                           uc.country) c) c) v;
+                           LEFT JOIN LATERAL ( SELECT DISTINCT lfe.language_id
+                                               FROM language_file_extensions lfe
+                                               WHERE lfe.extension = ANY (c.main_file_extensions)) lfe ON true
+                  GROUP BY ga.id) c) c) v;
 
 
 
