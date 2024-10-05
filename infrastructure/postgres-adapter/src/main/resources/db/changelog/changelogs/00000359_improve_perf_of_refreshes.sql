f52751737d3ca77d74ba7374153f0fa76bd4ee0d
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-10-05 09:28:08 +0200

    Improve v_contributor_global_data perf

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000359_improve_perf_of_refreshes.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000359_improve_perf_of_refreshes.sql
@@ -2,37 +2,48 @@ CREATE OR REPLACE VIEW bi.v_contributor_global_data AS
 SELECT v.*, md5(v::text) as hash
 FROM (SELECT c.*,
 
-             (select jsonb_agg(distinct jsonb_build_object('id', p.id,
-                                                           'slug', p.slug,
-                                                           'name', p.name,
-                                                           'logoUrl', p.logo_url))
+             (select jsonb_agg(jsonb_build_object('id', p.id,
+                                                  'slug', p.slug,
+                                                  'name', p.name,
+                                                  'logoUrl', p.logo_url))
               from projects p
               where p.id = any (c.project_ids))           as projects,
 
-             (select jsonb_agg(distinct jsonb_build_object('id', l.id,
-                                                           'slug', l.slug,
-                                                           'name', l.name,
-                                                           'logoUrl', l.logo_url,
-                                                           'bannerUrl', l.banner_url))
+             (select jsonb_agg(jsonb_build_object('id', l.id,
+                                                  'slug', l.slug,
+                                                  'name', l.name,
+                                                  'logoUrl', l.logo_url,
+                                                  'bannerUrl', l.banner_url))
               from languages l
               where l.id = any (c.language_ids))          as languages,
 
-             (select jsonb_agg(distinct jsonb_build_object('id', e.id,
-                                                           'slug', e.slug,
-                                                           'name', e.name,
-                                                           'logoUrl', e.logo_url,
-                                                           'bannerUrl', e.banner_url,
-                                                           'url', e.url))
+             (select jsonb_agg(jsonb_build_object('id', e.id,
+                                                  'slug', e.slug,
+                                                  'name', e.name,
+                                                  'logoUrl', e.logo_url,
+                                                  'bannerUrl', e.banner_url,
+                                                  'url', e.url))
               from ecosystems e
               where e.id = any (c.ecosystem_ids))         as ecosystems,
 
-             (select jsonb_agg(distinct jsonb_build_object('id', pc.id,
-                                                           'slug', pc.slug,
-                                                           'name', pc.name,
-                                                           'description', pc.description,
-                                                           'iconSlug', pc.icon_slug))
+             (select jsonb_agg(jsonb_build_object('id', pc.id,
+                                                  'slug', pc.slug,
+                                                  'name', pc.name,
+                                                  'description', pc.description,
+                                                  'iconSlug', pc.icon_slug))
               from project_categories pc
-              where pc.id = any (c.project_category_ids)) as categories
+              where pc.id = any (c.project_category_ids)) as categories,
+
+             (select jsonb_agg(distinct jsonb_build_object('id', prog.id,
+                                                           'name', prog.name)) as json
+              from programs prog
+              where prog.id = any (c.program_ids))        as programs,
+
+             (select jsonb_agg(distinct jsonb_build_object('id', cu.id,
+                                                           'code', cu.code,
+                                                           'name', cu.name)) as json
+              from currencies cu
+              where cu.id = any (c.currency_ids))         as currencies
 
       FROM (WITH ranked_project_github_repos_relationship AS (SELECT project_github_repos.project_id,
                                                                      project_github_repos.github_repo_id,
@@ -60,16 +71,29 @@ FROM (SELECT c.*,
                    array_agg(distinct ppc.project_category_id) filter ( where ppc.project_category_id is not null ) as project_category_ids,
                    array_agg(distinct lfe.language_id) filter ( where lfe.language_id is not null )                 as language_ids,
                    array_agg(distinct pe.ecosystem_id) filter ( where pe.ecosystem_id is not null )                 as ecosystem_ids,
-                   array_agg(distinct prog.id) filter ( where prog.id is not null )                                 as program_ids
+                   array_agg(distinct prog.id) filter ( where prog.id is not null )                                 as program_ids,
+                   array_agg(distinct currencies.id) filter ( where currencies.id is not null )                     as currency_ids,
+
+                   concat(coalesce(string_agg(distinct u.login, ' '), ''), ' ',
+                          coalesce(string_agg(distinct p.name, ' '), ''), ' ',
+                          coalesce(string_agg(distinct p.slug, ' '), ''), ' ',
+                          coalesce(string_agg(distinct pc.name, ' '), ''), ' ',
+                          coalesce(string_agg(distinct lfe.name, ' '), ''), ' ',
+                          coalesce(string_agg(distinct e.name, ' '), ''), ' ',
+                          coalesce(string_agg(distinct currencies.name, ' '), ''), ' ',
+                          coalesce(string_agg(distinct currencies.code, ' '), ''), ' ',
+                          coalesce(string_agg(distinct prog.name, ' '), ''))                                        as search
             FROM iam.all_users u
                      LEFT JOIN user_countries uc on uc.user_id = u.user_id
                      LEFT JOIN indexer_exp.contributions c ON c.contributor_id = u.github_user_id
                      LEFT JOIN ranked_project_github_repos_relationship pgr ON pgr.github_repo_id = c.repo_id AND pgr.row_number = 1
                      LEFT JOIN projects p on p.id = pgr.project_id
                      LEFT JOIN projects_ecosystems pe ON pe.project_id = p.id
+                     LEFT JOIN ecosystems e ON e.id = pe.ecosystem_id
                      LEFT JOIN v_programs_projects pp ON pp.project_id = p.id
                      LEFT JOIN programs prog ON prog.id = pp.program_id
                      LEFT JOIN projects_project_categories ppc ON ppc.project_id = p.id
+                     LEFT JOIN project_categories pc ON pc.id = ppc.project_category_id
                      LEFT JOIN rewards r on r.recipient_id = u.github_user_id
                      LEFT JOIN currencies on r.currency_id = currencies.id
                      LEFT JOIN LATERAL ( SELECT DISTINCT lfe_1.language_id,
