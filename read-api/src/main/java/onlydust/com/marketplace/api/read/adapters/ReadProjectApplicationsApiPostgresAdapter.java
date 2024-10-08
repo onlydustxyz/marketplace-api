7960c47dbcebffbf6b53564c054360b3a770eeaf
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-10-08 18:00:05 +0200

    /v2/applications for retro-compatibility

--- read-api/src/main/java/onlydust/com/marketplace/api/read/adapters/ReadProjectApplicationsApiPostgresAdapter.java
+++ read-api/src/main/java/onlydust/com/marketplace/api/read/adapters/ReadProjectApplicationsApiPostgresAdapter.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import onlydust.com.marketplace.api.contract.ReadProjectApplicationsApi;
 import onlydust.com.marketplace.api.contract.model.ApplicationsQueryParams;
 import onlydust.com.marketplace.api.contract.model.ProjectApplicationPageResponse;
+import onlydust.com.marketplace.api.contract.model.ProjectApplicationPageSort;
 import onlydust.com.marketplace.api.contract.model.ProjectApplicationResponse;
 import onlydust.com.marketplace.api.read.entities.project.ApplicationReadEntity;
 import onlydust.com.marketplace.api.read.repositories.ApplicationReadRepository;
@@ -13,6 +14,7 @@ import onlydust.com.marketplace.kernel.model.ProjectId;
 import onlydust.com.marketplace.project.domain.service.PermissionService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,8 +35,42 @@ public class ReadProjectApplicationsApiPostgresAdapter implements ReadProjectApp
     private final PermissionService permissionService;
     private final ApplicationReadRepository applicationReadRepository;
 
+
     @Override
-    public ResponseEntity<ProjectApplicationPageResponse> getProjectsApplications(ApplicationsQueryParams params) {
+    public ResponseEntity<ProjectApplicationPageResponse> getProjectsApplications(Integer pageIndex,
+                                                                                  Integer pageSize, UUID projectId,
+                                                                                  Long issueId,
+                                                                                  Long applicantId,
+                                                                                  Boolean isApplicantProjectMember,
+                                                                                  String applicantLoginSearch,
+                                                                                  ProjectApplicationPageSort sort) {
+        if (projectId == null && applicantId == null) {
+            throw OnlyDustException.badRequest("At least one of projectId and applicantId must be provided");
+        }
+
+        final var caller = authenticatedAppUserService.getAuthenticatedUser();
+        if (!caller.githubUserId().equals(applicantId) && !permissionService.isUserProjectLead(ProjectId.of(projectId), caller.id())) {
+            throw forbidden("Only project leads can get project applications");
+        }
+
+        final var page = applicationReadRepository.findAll(projectId,
+                issueId,
+                applicantId,
+                isApplicantProjectMember,
+                applicantLoginSearch,
+                PageRequest.of(pageIndex, pageSize, JpaSort.unsafe("element(applicant.globalUsersRanks).rank")));
+
+        return ok(new ProjectApplicationPageResponse()
+                .totalItemNumber((int) page.getTotalElements())
+                .totalPageNumber(page.getTotalPages())
+                .nextPageIndex(page.hasNext() ? pageIndex + 1 : pageIndex)
+                .hasMore(page.hasNext())
+                .applications(page.getContent().stream().map(ApplicationReadEntity::toPageItemDto).toList())
+        );
+    }
+
+    @Override
+    public ResponseEntity<ProjectApplicationPageResponse> getProjectsApplicationsV2(ApplicationsQueryParams params) {
         if (params.getProjectId() == null && params.getApplicantId() == null) {
             throw OnlyDustException.badRequest("At least one of projectId and applicantId must be provided");
         }
@@ -49,7 +85,7 @@ public class ReadProjectApplicationsApiPostgresAdapter implements ReadProjectApp
                 params.getIssueId(),
                 params.getApplicantId(),
                 params.getIsApplicantProjectMember(),
-                params.getC().getSearch(),
+                params.getU().getSearch(),
                 PageRequest.of(params.getPageIndex(), params.getPageSize()));
 
         return ok(new ProjectApplicationPageResponse()
