db78161d98097a7726e18942977b9d0e438dd7c3
Olivier Fuxet <ofux@users.noreply.github.com>
2024-10-10 09:33:32 +0200

    load tests authenticated (#1201)
    * Introduce authenticated load-tests
    
    * wip
    
    * wip
    
    * wip
    
    * Add homepage requests
    
    * wip
    
    * wip

---
+++ load-tests/k6/authenticated-users.js
@@ -0,0 +1,133 @@
+// Run with: K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=html-report.html k6 run load-tests/k6/authenticated-users.js
+// For datadog: K6_STATSD_ENABLE_TAGS=true k6 run --tag test_run_id=1 --out output-statsd load-tests/k6/authenticated-users.js
+
+import {check, sleep} from 'k6';
+import http from 'k6/http';
+import exec from 'k6/execution';
+import {userIds as userIdsDevelop} from "./users/develop.js";
+import {userIds as userIdsStaging} from "./users/staging.js";
+import {userIds as userIdsProduction} from "./users/production.js";
+
+export const options = {
+    scenarios: {
+        odhack: {
+            executor: 'ramping-vus',
+            startVUs: 1,
+            stages: [
+                {duration: '120s', target: 5000},
+                {duration: '30s', target: 0},
+            ],
+            gracefulRampDown: '0s',
+        },
+    },
+};
+
+const ENV = 'perf';
+const BASE_URL = `https://${ENV}-api.onlydust.com/api/v1`;//'http://localhost:9999/api/v1';
+const ACCESS_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InlvcHBLQzlTTFptT1pBbXhtV2J6NiJ9.eyJpc3MiOiJodHRwczovL3Byb2R1Y3Rpb24tb25seWR1c3QuZXUuYXV0aDAuY29tLyIsInN1YiI6ImdpdGh1Ynw1OTU1MDUiLCJhdWQiOlsiaHR0cHM6Ly9hcGkub25seWR1c3QuY29tL2FwaSIsImh0dHBzOi8vcHJvZHVjdGlvbi1vbmx5ZHVzdC5ldS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzI4NDkzMTk4LCJleHAiOjE3Mjg0OTQzOTgsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgb2ZmbGluZV9hY2Nlc3MiLCJhenAiOiJ2TkxwYU1EQU1RZVIwZHJhdkxreHdZdlZpVGRXYzZUSSJ9.Qaoh2wS8cFCzb86QVqmXe3_6zPR3oxYV6XXpD2jD8rSEqPd8jyLf0Xa6zKaTsV7WOfoHPrFAmIWu6bY33EL9gcOJUIBtYrFi6L5ekigJLkTlzv-V7wWvWSSFGqXkmKGopwIgxhmbsfFyrFjWUuqNlTeSjiyjBl78sz3a5E2klafvrldZBCMpC2GiWx1Zy-jonHEoCUVmCf0dIrptEHEVgZJJEWc1TxFn4zvc69Gu0ragE9ekpzBqwpDMCCLrMQeIyWKcUtXTChrxL1IRpk_GSkrFSoDF9ir0tNMUZQ1WU_GI4Rn__z_toyN8VYuP4ObaaE9B7tySdw5bnul4W_YfSw';
+const HACKATHON_SLUG = 'odhack-80';
+
+
+function userIds() {
+    return ENV === 'perf' ? userIdsProduction : ENV === 'staging' ? userIdsStaging : userIdsDevelop;
+}
+
+function userId() {
+    return userIds()[exec.vu.idInTest % userIds().length];
+}
+
+function idle(min, max) {
+    sleep(Math.random() * (max - min) + min);
+}
+
+
+export default function () {
+    const params = {
+        headers: {
+            'Authorization': `Bearer ${ACCESS_TOKEN}`,
+            'X-Impersonation-Claims': `{"sub":"github|${userId()}"}`,
+            'Content-Type': 'application/json',
+        },
+    };
+
+    console.debug(`User ${userId()} started`);
+    notificationRequests(params);
+    //http.get(`${BASE_URL}/me/recommended-projects`, params);
+    sleep(5);
+    // globalRequests(params);
+    // homeRequests(params);
+    //
+    // idle(3, 10);
+    // globalRequests(params);
+    // hackathonsRequests(params);
+}
+
+function globalRequests(params) {
+    http.get(`${BASE_URL}/me/billing-profiles`, params);
+    console.debug(`User ${userId()} first global requests done`);
+    http.get(`${BASE_URL}/me/profile`, params);
+    http.get(`${BASE_URL}/me/onboarding`, params);
+    http.get(`${BASE_URL}/banner?hiddenIgnoredByMe=true`, params);
+    notificationRequests(params);
+}
+
+function notificationRequests(params) {
+    http.get(`${BASE_URL}/me/notifications/count?status=UNREAD`, params);
+}
+
+function homeRequests(params) {
+    http.get(`${BASE_URL}/me/rewards`, params);
+    http.get(`${BASE_URL}/me/projects`, params);
+    http.get(`${BASE_URL}/me/recommended-projects`, params);
+    http.get(`${BASE_URL}/projects`, params);
+    http.get(`${BASE_URL}/public-activity`, params);
+    console.debug('Home requests done');
+}
+
+function hackathonsRequests(params) {
+    http.get(`${BASE_URL}/hackathons`, params);
+    idle(1, 10);
+
+    const hackathon = http.get(`${BASE_URL}/hackathons/slug/${HACKATHON_SLUG}`, params);
+    const hackathonId = hackathon.json().id;
+    console.debug(`Selected hackathon: ${HACKATHON_SLUG} (id: ${hackathonId})`);
+    registerToHackathon(hackathonId, params);
+    hackathonRequests(hackathonId, params);
+}
+
+function registerToHackathon(hackathonId, params) {
+    idle(10, 15);
+    const profilePatchRes = http.patch(`${BASE_URL}/me/profile`, JSON.stringify({
+        "contacts": [
+            {
+                "channel": "TELEGRAM",
+                "contact": "t.me/foo",
+                "visibility": "public"
+            }
+        ]
+    }), params);
+    check(profilePatchRes, {
+        'successfully patched profile': (r) => r.status === 204,
+    });
+
+    const res = http.put(`${BASE_URL}/me/hackathons/${hackathonId}/registrations`, null, params);
+    check(res, {
+        'successfully registered to hackathon': (r) => r.status === 204,
+    });
+    console.debug(`User ${userId()} registered to hackathon ${hackathonId}`);
+}
+
+function hackathonRequests(hackathonId, params) {
+    const hackathonProjectIssues = http.get(`${BASE_URL}/hackathons/${hackathonId}/project-issues?statuses=OPEN`, params);
+    const projectIds = hackathonProjectIssues.json().projects.map(p => p.project.id);
+    console.debug(`Hackathon projects: ${projectIds}`);
+    projectIds.forEach(projectId => hackathonProjectsRequests(hackathonId, projectId, params));
+}
+
+function hackathonProjectsRequests(hackathonId, projectId, params) {
+    idle(10, 20);
+    const res = http.get(`${BASE_URL}/projects/${projectId}/public-issues?statuses=OPEN&hackathonId=${hackathonId}`, params);
+    const projectIssueCount = res.json().totalItemNumber;
+    console.debug(`Fetched ${projectIssueCount} issues from project ${projectId}`);
+    notificationRequests(params);
+}
