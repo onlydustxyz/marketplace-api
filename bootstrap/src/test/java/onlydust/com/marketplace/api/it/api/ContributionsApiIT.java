00b07522a78d2a3d804c087788a2be0e86291c49
Anthony Buisset <abuisset@gmail.com>
2024-10-16 11:52:31 +0200

    sorting

--- bootstrap/src/test/java/onlydust/com/marketplace/api/it/api/ContributionsApiIT.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/it/api/ContributionsApiIT.java
@@ -1,9 +1,19 @@
 package onlydust.com.marketplace.api.it.api;
 
+import onlydust.com.marketplace.api.contract.model.ContributionActivityPageItemResponse;
+import onlydust.com.marketplace.api.contract.model.ContributionActivityPageResponse;
 import onlydust.com.marketplace.api.suites.tags.TagProject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Comparator;
 import java.util.Map;
+import java.util.function.Consumer;
+
+import static java.util.Collections.reverse;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @TagProject
@@ -20,44 +30,48 @@ public class ContributionsApiIT extends AbstractMarketplaceApiIT {
                 .expectBody()
                 .json("""
                         {
+                          "totalPageNumber": 57700,
+                          "totalItemNumber": 57700,
+                          "hasMore": true,
+                          "nextPageIndex": 1,
                           "contributions": [
                             {
-                              "type": "CODE_REVIEW",
+                              "type": "PULL_REQUEST",
                               "repo": {
-                                "id": 350360184,
-                                "owner": "calcom",
-                                "name": "cal.com",
-                                "description": "Scheduling infrastructure for absolutely everyone.",
-                                "htmlUrl": "https://github.com/calcom/cal.com"
+                                "id": 40652912,
+                                "owner": "IonicaBizau",
+                                "name": "node-cobol",
+                                "description": ":tv: COBOL bridge for NodeJS which allows you to run COBOL code from NodeJS.",
+                                "htmlUrl": "https://github.com/IonicaBizau/node-cobol"
                               },
                               "githubAuthor": {
-                                "githubUserId": 22683064,
-                                "login": "shivamklr",
-                                "avatarUrl": "https://avatars.githubusercontent.com/u/22683064?v=4"
+                                "githubUserId": 1717382,
+                                "login": "BernhardBehrendt",
+                                "avatarUrl": "https://avatars.githubusercontent.com/u/1717382?v=4"
                               },
-                              "githubId": 1503740037,
-                              "githubNumber": 11166,
-                              "githubStatus": "PENDING",
-                              "githubTitle": "chore: add Wizard in storybook (CALCOM-10760)",
-                              "githubHtmlUrl": "https://github.com/calcom/cal.com/pull/11166",
-                              "githubBody": "Here is the GitStart Ticket for this pull request: [CALCOM-10760](https://developers.gitstart.com/client/calcom/ticket/CALCOM-10760)\\r\\n\\r\\n- Please mark your PR as \\"ready for review\\" when you have completed all requirements.\\r\\n- Please ensure that your PR is reviewed by at least 2 people after you have completed it.\\r\\n- Comments for review or requested changes from the upstream client will be notified to you by email and will also show on the [ticket page](https://developers.gitstart.com/client/calcom/ticket/CALCOM-10760).\\r\\n- After the PR is merged on the upstream, please merge this PR.\\r\\n- If there exist merge conflicts on your current PR and you have to close the PR instead of merging it, please go to the [ticket page](https://developers.gitstart.com/client/calcom/ticket/CALCOM-10760) to manually finish the task that is related to this PR.\\r\\n\\r\\nHappy hacking! \\uD83C\\uDF89\\r\\n\\r\\n## DEMO\\r\\n\\r\\nhttps://www.loom.com/share/29c39c63054b40be880f1ea34ce35ff9?sid=71d9de65-b996-4649-b245-136d8dc7b7bd\\r\\n\\r\\nWe added an explanation about why we can't use the next button to go to another step:\\r\\n\\r\\n![image](https://github.com/calcom/cal.com/assets/121884634/24c354a3-305b-44e4-996d-7b4276e94643)\\r\\n\\r\\nWe already have a storybook file for steps component so we can test it there",
+                              "githubId": 42703857,
+                              "githubNumber": 3,
+                              "githubStatus": "MERGED",
+                              "githubTitle": "German press article",
+                              "githubHtmlUrl": "https://github.com/IonicaBizau/node-cobol/pull/3",
+                              "githubBody": "by Alexander Neumann published on heise online\\n",
                               "githubLabels": null,
-                              "lastUpdatedAt": "2023-09-06T00:03:24Z",
-                              "id": "00002090335968f5e3c802d4fca91e48c621e3a34ba03e24ca440721d52ba5a2",
-                              "createdAt": "2023-09-06T02:03:24Z",
-                              "completedAt": "2023-09-06T15:22:03Z",
+                              "lastUpdatedAt": "2023-11-24T10:32:31.695306Z",
+                              "id": "93545fddecd2e42b0fccd23f62415dab09e37ef3ea1b131e68a472b67062cd48",
+                              "createdAt": "2015-08-18T14:19:08Z",
+                              "completedAt": "2015-08-18T16:25:53Z",
                               "activityStatus": "DONE",
                               "project": {
-                                "id": "1bdddf7d-46e1-4a3f-b8a3-85e85a6df59e",
-                                "slug": "calcom",
-                                "name": "Cal.com",
-                                "logoUrl": "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/5271998260751715005.png"
+                                "id": "a852e8fd-de3c-4a14-813e-4b592af40d54",
+                                "slug": "onlydust-marketplace",
+                                "name": "OnlyDust Marketplace",
+                                "logoUrl": null
                               },
                               "contributors": [
                                 {
-                                  "githubUserId": 22683064,
-                                  "login": "shivamklr",
-                                  "avatarUrl": "https://avatars.githubusercontent.com/u/22683064?v=4"
+                                  "githubUserId": 1717382,
+                                  "login": "BernhardBehrendt",
+                                  "avatarUrl": "https://avatars.githubusercontent.com/u/1717382?v=4"
                                 }
                               ],
                               "applicants": null,
@@ -68,5 +82,44 @@ public class ContributionsApiIT extends AbstractMarketplaceApiIT {
                           ]
                         }
                         """);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "CREATED_AT,ASC",
+            "CREATED_AT,DESC",
+            "TYPE,ASC",
+            "TYPE,DESC"
+    })
+    void should_get_contributions_with_sorting(String sort, String direction) {
+        // When
+        assertContributions(Map.of("sort", sort, "sortDirection", direction), r -> {
+            // Then
+            final var contributions = r.getContributions();
+            if (direction.equals("DESC"))
+                reverse(contributions);
+
+            switch (sort) {
+                case "CREATED_AT":
+                    assertThat(contributions).isSortedAccordingTo(Comparator.comparing(ContributionActivityPageItemResponse::getCreatedAt));
+                    break;
+                case "TYPE":
+                    assertThat(contributions).isSortedAccordingTo(Comparator.comparing(ContributionActivityPageItemResponse::getType));
+                    break;
+            }
+        });
+    }
+
+    private WebTestClient.BodySpec<ContributionActivityPageResponse, ?> assertContributions(Map<String, String> params,
+                                                                                            Consumer<ContributionActivityPageResponse> asserter) {
+        return client.get()
+                .uri(getApiURI(CONTRIBUTIONS, params))
+                // Then
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(ContributionActivityPageResponse.class)
+                .consumeWith(r -> asserter.accept(r.getResponseBody()))
+                ;
     }
 }
