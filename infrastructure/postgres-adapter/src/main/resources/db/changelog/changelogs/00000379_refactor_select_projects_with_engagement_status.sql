1c0b0ac7e44ce0871019e769d65cd3a0c0ed416a
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-11-05 15:08:31 +0100

    Add engagement_status to GET bi/projects

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000379_refactor_select_projects_with_engagement_status.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000379_refactor_select_projects_with_engagement_status.sql
@@ -1,1 +1,16 @@
 alter type contributor_activity_status rename to engagement_status;
+
+create index on bi.p_contribution_data (project_id, contribution_status, timestamp);
+
+DROP FUNCTION bi.select_projects(fromDate timestamptz,
+                                 toDate timestamptz,
+                                 dataSourceIds uuid[],
+                                 programIds uuid[],
+                                 projectIds uuid[],
+                                 projectSlugs text[],
+                                 projectLeadIds uuid[],
+                                 categoryIds uuid[],
+                                 languageIds uuid[],
+                                 ecosystemIds uuid[],
+                                 searchQuery text,
+                                 showFilteredKpis boolean);
