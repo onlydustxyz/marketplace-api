462dc0b0b0397e886a9f72fa04c6719ced62096d
PierreOucif <pierre.oucif@gadz.org>
2024-11-07 11:42:10 +0100

    Update dockerfile

--- docker-start-api.sh
+++ docker-start-api.sh
@@ -5,11 +5,25 @@ if [ -z "$SPRING_PROFILES_ACTIVE" ]; then
   echo "ERROR - Missing spring-profiles-active parameter"
   exit 1
 fi
+if [ -z "$DD_ENV" ]; then
+  echo "ERROR - Missing datadog env parameter"
+  exit 1
+fi
 
-echo "Starting marketplace-api for spring-profiles-active ${SPRING_PROFILES_ACTIVE}"
 java_options="-server -XX:MaxRAMPercentage=75.0 -XX:MaxMetaspaceSize=256m -XX:+UseG1GC -Xss100m \
-               -XX:FlightRecorderOptions=stackdepth=256 -Djava.security.egd=file:/dev/urandom \
-                -Dliquibase.changelogLockPollRate=1 -Duser.timezone=\"Europe/Paris\""
-java $java_options \
-  -jar webapp/marketplace-api.jar \
-  --spring.profiles.active=$SPRING_PROFILES_ACTIVE
+                 -XX:FlightRecorderOptions=stackdepth=256 -Djava.security.egd=file:/dev/urandom \
+                  -Dliquibase.changelogLockPollRate=1 -Duser.timezone=\"Europe/Paris\""
+
+if [[ $DD_ENV == "aws" ]]; then
+  echo "Starting marketplace-api for spring-profiles-active ${SPRING_PROFILES_ACTIVE} and Datadog env ${DD_ENV}"
+  java_options="${java_options} -Ddd.profiling.enabled=true -Ddd.logs.injection=true"
+  java -javaagent:/webapp/dd-java-agent.jar \
+    $java_options \
+    -jar webapp/marketplace-api.jar \
+    --spring.profiles.active=$SPRING_PROFILES_ACTIVE
+else
+  echo "Starting marketplace-api for spring-profiles-active ${SPRING_PROFILES_ACTIVE}"
+  java $java_options \
+    -jar webapp/marketplace-api.jar \
+    --spring.profiles.active=$SPRING_PROFILES_ACTIVE
+fi
