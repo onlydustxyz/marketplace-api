f086cffa0d32bb31e1126a43406a57eac66b077a
PierreOucif <pierre.oucif@gadz.org>
2024-11-13 18:22:16 +0100

    Revert dump

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000011_init_mandatory_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.currencies (id, type, name, code, logo_url, decimals, description, country_restrictions, cmc_id)
-VALUES (gen_random_uuid(), 'FIAT', 'US Dollar', 'USD', 'https://staging-onlydust-app-images.s3.eu-west-1.amazonaws.com/f171e9690f6658e106a049cd62843ec4.png', 2,
-        null, '{}', 2781);
