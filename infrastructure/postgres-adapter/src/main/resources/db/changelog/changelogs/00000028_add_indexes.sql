9bebd0cd07e26b0eb5c7f6be65cdf40088a35d7b
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-11-21 12:05:47 +0100

    Add indexes suggested by Datadog

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000028_add_indexes.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000028_add_indexes.sql
@@ -1,3 +1,6 @@
 create index if not exists notifications_data_notification_type_issue_id_index
     on iam.notifications ((data -> 'notification' ->> '@type'), (cast(data -> 'notification' -> 'issue' ->> 'id' as bigint)));
 
+-- Not related to the feature, but recommended by Datadog
+create index if not exists p_contribution_data_status_type_uuid_pid_pslug_rid_timestamp
+    on bi.p_contribution_data (activity_status, contribution_type, contribution_uuid, project_id, project_slug, repo_id, timestamp);
