5a084b93c8ed7c63510e190725886661b87c1a7d
Olivier Fuxet <ofux@users.noreply.github.com>
2024-11-22 10:47:02 +0100

    e-2479-problemes-de-performance-avec-les-assignations-et-github (#1304)
    * Fix application_data refresh perf
    
    * Try to make AsyncNotificationEmailProcessor actually async

--- user-domain/src/main/java/onlydust/com/marketplace/user/domain/service/AsyncNotificationEmailProcessor.java
+++ user-domain/src/main/java/onlydust/com/marketplace/user/domain/service/AsyncNotificationEmailProcessor.java
@@ -8,9 +8,9 @@ import onlydust.com.marketplace.user.domain.model.NotificationRecipient;
 import onlydust.com.marketplace.user.domain.model.SendableNotification;
 import onlydust.com.marketplace.user.domain.port.output.NotificationSender;
 import onlydust.com.marketplace.user.domain.port.output.NotificationStoragePort;
-import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @AllArgsConstructor
@@ -19,26 +19,28 @@ public class AsyncNotificationEmailProcessor implements NotificationSender {
     private final NotificationSender notificationSender;
     private final NotificationStoragePort notificationStoragePort;
 
-    @Async
     @Override
     public void send(SendableNotification notification) {
-        try {
-            notificationSender.send(notification);
-            notificationStoragePort.markAsSent(NotificationChannel.EMAIL, List.of(notification.id()));
-        } catch (Exception e) {
-            LOGGER.error("Error while sending email for notification %s".formatted(notification.id()), e);
-        }
+        CompletableFuture.runAsync(() -> {
+            try {
+                notificationSender.send(notification);
+                notificationStoragePort.markAsSent(NotificationChannel.EMAIL, List.of(notification.id()));
+            } catch (Exception e) {
+                LOGGER.error("Error while sending email for notification %s".formatted(notification.id()), e);
+            }
+        });
     }
 
     @Override
-    @Async
     public void sendAllForRecipient(NotificationRecipient notificationRecipient, List<SendableNotification> sendableNotifications) {
-        try {
-            notificationSender.sendAllForRecipient(notificationRecipient, sendableNotifications);
-            notificationStoragePort.markAsSent(NotificationChannel.SUMMARY_EMAIL, sendableNotifications.stream().map(Notification::id).toList());
-        } catch (Exception e) {
-            LOGGER.error("Error while sending summary email for recipient %s and %s notification(s)".formatted(notificationRecipient,
-                    sendableNotifications.size()), e);
-        }
+        CompletableFuture.runAsync(() -> {
+            try {
+                notificationSender.sendAllForRecipient(notificationRecipient, sendableNotifications);
+                notificationStoragePort.markAsSent(NotificationChannel.SUMMARY_EMAIL, sendableNotifications.stream().map(Notification::id).toList());
+            } catch (Exception e) {
+                LOGGER.error("Error while sending summary email for recipient %s and %s notification(s)".formatted(notificationRecipient,
+                        sendableNotifications.size()), e);
+            }
+        });
     }
 }
