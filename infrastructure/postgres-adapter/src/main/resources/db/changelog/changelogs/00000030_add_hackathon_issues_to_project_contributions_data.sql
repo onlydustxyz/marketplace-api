e0219f41135afeb8c139686d91f366f61e36de78
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-11-25 10:45:43 +0100

    Iso project_contributions_data

---
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000030_add_hackathon_issues_to_project_contributions_data.sql
@@ -0,0 +1,17 @@
+call drop_pseudo_projection('bi', 'project_contributions_data');
+
+call create_pseudo_projection('bi', 'project_contributions_data', $$
+SELECT p.id                                                                            as project_id,
+       array_agg(distinct cd.repo_id)                                                  as repo_ids,
+       count(distinct cd.contributor_id)                                               as contributor_count,
+       count(distinct cd.contribution_uuid) filter ( where cd.is_good_first_issue and
+                                                coalesce(array_length(cd.assignee_ids, 1), 0) = 0 and
+                                                cd.contribution_status != 'COMPLETED' and
+                                                cd.contribution_status != 'CANCELLED') as good_first_issue_count
+FROM projects p
+         LEFT JOIN bi.p_per_contributor_contribution_data cd ON cd.project_id = p.id
+GROUP BY p.id
+$$, 'project_id');
+
+create unique index on bi.p_project_contributions_data (project_id, contributor_count, good_first_issue_count);
+create index on bi.p_project_contributions_data using gin (repo_ids);
