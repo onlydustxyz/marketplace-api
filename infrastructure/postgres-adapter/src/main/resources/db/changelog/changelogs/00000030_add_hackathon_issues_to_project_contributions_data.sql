074c0ab03b6772d9f0112b8148d8c1bf49d8f206
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-11-25 11:16:55 +0100

    Add live_hackathon_issue_count to project_contributions_data

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000030_add_hackathon_issues_to_project_contributions_data.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000030_add_hackathon_issues_to_project_contributions_data.sql
@@ -1,17 +1,25 @@
 call drop_pseudo_projection('bi', 'project_contributions_data');
 
 call create_pseudo_projection('bi', 'project_contributions_data', $$
-SELECT p.id                                                                            as project_id,
-       array_agg(distinct cd.repo_id)                                                  as repo_ids,
-       count(distinct cd.contributor_id)                                               as contributor_count,
+SELECT p.id                                                                                       as project_id,
+       count(distinct cd.contributor_id)                                                          as contributor_count,
        count(distinct cd.contribution_uuid) filter ( where cd.is_good_first_issue and
-                                                coalesce(array_length(cd.assignee_ids, 1), 0) = 0 and
-                                                cd.contribution_status != 'COMPLETED' and
-                                                cd.contribution_status != 'CANCELLED') as good_first_issue_count
+                                                           coalesce(array_length(cd.assignee_ids, 1), 0) = 0 and
+                                                           cd.contribution_status != 'COMPLETED' and
+                                                           cd.contribution_status != 'CANCELLED') as good_first_issue_count,
+       count(distinct cd.contribution_uuid) filter ( where gl.id is not null and
+                                                           coalesce(array_length(cd.assignee_ids, 1), 0) = 0 and
+                                                           cd.contribution_status != 'COMPLETED' and
+                                                           cd.contribution_status != 'CANCELLED') as live_hackathon_issue_count
 FROM projects p
          LEFT JOIN bi.p_per_contributor_contribution_data cd ON cd.project_id = p.id
+         LEFT JOIN hackathon_projects hp ON hp.project_id = p.id
+         LEFT JOIN hackathons h ON h.id = hp.hackathon_id AND
+                                   h.status = 'PUBLISHED' AND
+                                   h.start_date <= now() AND
+                                   h.end_date >= now()
+         LEFT JOIN indexer_exp.github_labels gl ON gl.name = any (h.github_labels) AND gl.id = any (cd.github_label_ids)
 GROUP BY p.id
 $$, 'project_id');
 
 create unique index on bi.p_project_contributions_data (project_id, contributor_count, good_first_issue_count);
-create index on bi.p_project_contributions_data using gin (repo_ids);
