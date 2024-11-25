49daa9630494ae9add6c7b9bf04303f4c917c7e8
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-11-25 14:12:35 +0100

    Fix project_contributions_data

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000030_add_hackathon_issues_to_project_contributions_data.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000030_add_hackathon_issues_to_project_contributions_data.sql
@@ -2,17 +2,19 @@ call drop_pseudo_projection('bi', 'project_contributions_data');
 
 call create_pseudo_projection('bi', 'project_contributions_data', $$
 SELECT p.id                                                                                       as project_id,
-       count(distinct cd.contributor_id)                                                          as contributor_count,
+       count(distinct unnested.contributor_ids)                                                   as contributor_count,
        count(distinct cd.contribution_uuid) filter ( where cd.is_good_first_issue and
-                                                           coalesce(array_length(cd.assignee_ids, 1), 0) = 0 and
+                                                           coalesce(array_length(ccd.assignee_ids, 1), 0) = 0 and
                                                            cd.contribution_status != 'COMPLETED' and
                                                            cd.contribution_status != 'CANCELLED') as good_first_issue_count,
        count(distinct cd.contribution_uuid) filter ( where gl.id is not null and
-                                                           coalesce(array_length(cd.assignee_ids, 1), 0) = 0 and
+                                                           coalesce(array_length(ccd.assignee_ids, 1), 0) = 0 and
                                                            cd.contribution_status != 'COMPLETED' and
                                                            cd.contribution_status != 'CANCELLED') as live_hackathon_issue_count
 FROM projects p
-         LEFT JOIN bi.p_per_contributor_contribution_data cd ON cd.project_id = p.id
+         LEFT JOIN bi.p_contribution_data cd ON cd.project_id = p.id
+         LEFT JOIN bi.p_contribution_contributors_data ccd ON ccd.contribution_uuid = cd.contribution_uuid
+         LEFT JOIN unnest(ccd.contributor_ids) unnested(contributor_ids) ON true
          LEFT JOIN hackathon_projects hp ON hp.project_id = p.id
          LEFT JOIN hackathons h ON h.id = hp.hackathon_id AND
                                    h.status = 'PUBLISHED' AND
