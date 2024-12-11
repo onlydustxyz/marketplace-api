317a20a2ccebf5fca0cff6290c89f9510460a5e5
Ilysse <pierre.oucif@gadz.org>
2024-12-11 21:01:56 +0100

    Fix contributors indexation count (#1351)

--- bootstrap/src/test/java/onlydust/com/marketplace/api/suites/SearchSuite.java
+++ bootstrap/src/test/java/onlydust/com/marketplace/api/suites/SearchSuite.java
@@ -1,6 +1,5 @@
 package onlydust.com.marketplace.api.suites;
 
-import onlydust.com.marketplace.api.suites.tags.TagProject;
 import onlydust.com.marketplace.api.suites.tags.TagSearch;
 import org.junit.platform.suite.api.IncludeTags;
 
