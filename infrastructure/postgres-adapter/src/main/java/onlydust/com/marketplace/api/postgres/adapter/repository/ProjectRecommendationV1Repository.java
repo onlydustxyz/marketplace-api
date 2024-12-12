24ed49c42dc9cf37007c299e0795663b86e8ad39
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-12-12 14:49:22 +0100

    Add matching query + "reranking" algo

--- infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/ProjectRecommendationV1Repository.java
+++ infrastructure/postgres-adapter/src/main/java/onlydust/com/marketplace/api/postgres/adapter/repository/ProjectRecommendationV1Repository.java
@@ -33,4 +33,39 @@ public interface ProjectRecommendationV1Repository extends JpaRepository<Project
             LIMIT :limit
             """, nativeQuery = true)
     List<ProjectRecommendationEntity> findTopProjects(int limit);
+
+    @Query(value = """
+            select d.project_id,
+                   d.language_score + d.ecosystem_score + d.experience_score + d.maturity_score + d.community_score as score
+            from (select p.project_id,
+                         p.project_name,
+                         (case
+                              when ua.languages is null or array_length(ua.languages, 1) = 0 or ua.languages && ARRAY ['00000000-0000-0000-0000-000000000000']::uuid[] then 0
+                              when ua.learning_preference = 1 then 100.0
+                              when ua.learning_preference = 2 then 10.0
+                              else 0 end) * (clamp(uap.common_languages_count, 0, 1)) as language_score,
+                         100.0 * (case
+                                      when ua.ecosystems is null or array_length(ua.ecosystems, 1) = 0 or ua.ecosystems && ARRAY ['00000000-0000-0000-0000-000000000000']::uuid[] then 0
+                                      else uap.common_ecosystems_count_normalized end)         as ecosystem_score,
+                         5.0 * (case
+                                    when ua.learning_preference = 1 then 1 - abs(uap.experience_level_diff) / 3.0
+                                    when ua.learning_preference = 2 then 1 - abs(uap.experience_level_diff + 1) / 3.0
+                                    else 0 end)                                                as experience_score,
+                         5.0 * (case
+                                    when ua.project_maturity = 1 then pe.well_established_score_normalized
+                                    when ua.project_maturity = 2 then pe.emerging_score_normalized
+                                    else 0 end)                                                as maturity_score,
+                         (case
+                              when ua.community_importance = 1 then 3.0
+                              when ua.community_importance = 2 then 6.0
+                              else 0 end) * pe.active_community_score_normalized               as community_score
+                  from bi.p_project_global_data p
+                           join reco.projects_computed_data pc on p.project_id = pc.project_id
+                           join reco.projects_extrapolated_data pe on p.project_id = pe.project_id
+                           join reco.user_answers_v1 ua on ua.user_id = :userId
+                           join reco.user_answers_to_projects_data uap on uap.project_id = p.project_id and uap.user_id = ua.user_id) d
+            order by score desc
+            LIMIT :limit
+            """, nativeQuery = true)
+    List<ProjectRecommendationEntity> findTopMatchingProjects(UUID userId, int limit);
 } 
